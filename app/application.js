c69847ad8de97c703bc745bf92bdff1d3459b6b0
Cameron Bothner <cbothner@umich.edu>
2015-12-31 12:01:35 -0500

    React workspace. Hello world.

---
+++ app/application.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render( <h1>Michigan Sustainability Cases</h1>, document.getElementById('container'));
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  entry: './app/application.js',
+  output: {
+    path: './build/',
+    filename: 'index.js'
+  },
+  devServer: {
+    inline: true,
+    port: 3333
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }, {
+        test: /\.scss$/,
+        loader: 'style!css!sass'
+      }
+    ]
+  }
+};
