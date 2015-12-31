fdf22e5b54a6a7504317845c8ce78846f1464e3a
Cameron Bothner <cbothner@umich.edu>
2015-12-31 14:37:18 -0500

    Factors out CaseReader

---
+++ app/CaseReader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './CaseReader.scss';
+
+class CaseReader extends React.Component {
+  render () { return (
+    <h1>Wolf Case</h1>
+  ) }
+}
+
+export default CaseReader
--- app/application.js
+++ app/application.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render( <h1>Michigan Sustainability Cases</h1>, document.getElementById('container'));
+import CaseReader from './CaseReader.js'
+
+ReactDOM.render( <CaseReader />, document.getElementById('container'));
