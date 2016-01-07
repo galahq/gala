f179b0f55edc0c754dd4b84b7c0f105a7b3f1c3f
Cameron Bothner <cbothner@umich.edu>
2016-01-07 15:05:43 -0500

    Ensures 100% height and indexredirect to chapter 0

--- app/application.js
+++ app/application.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router'
+import { Router, Route, IndexRedirect, browserHistory } from 'react-router'
 
 window.$ = require('jquery');
 
 import CaseReader from './components/CaseReader.js'
 import Narrative from './components/Narrative.js'
-
-import './stylesheets/main.scss';
 
 //ReactDOM.render(
   //<CaseReader id="497" />
@@ -16,7 +14,8 @@ import './stylesheets/main.scss';
     //<Route path="/read/:id(/chapter/:chapter)" component={CaseReader} />
 ReactDOM.render((
   <Router history={browserHistory}>
-    <Route path="/read/:id/" component={CaseReader}>
+    <Route path="/read/:id(/)" component={CaseReader}>
+      <IndexRedirect to="0" />
       <Route path=":chapter" component={Narrative} />
     </Route>
   </Router>
