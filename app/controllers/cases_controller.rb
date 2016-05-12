5cef2d0d99fced502c6a7324cf07b5b7c506335b
Cameron Bothner <cbothner@umich.edu>
2016-05-12 17:09:37 -0400

    Factors out the token_authentication method.

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -41,14 +41,6 @@ class CasesController < ApplicationController
   end
 
   private
-    def authenticate_reader_from_token!
-      reader_email = params[:reader_email].presence
-      reader = reader_email && Reader.find_by_email(reader_email)
-      if reader && Devise.secure_compare(reader.authentication_token, params[:reader_token])
-        sign_in reader, store: false
-      end
-    end
-
     # Use callbacks to share common setup or constraints between actions.
     def set_case
       @case = Case.find_by_slug params[:slug]
