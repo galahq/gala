c30254e990a1dbfc15ad20df370e795df9bd10cd
Cameron Bothner <cbothner@umich.edu>
2016-05-27 18:12:24 -0400

    Removes GalaController b/c it's integrated now.

--- app/controllers/gala_controller.rb
+++
@@ -1,5 +0,0 @@
-class GalaController < ApplicationController
-  def open
-    redirect_to "http://#{Orchard::Application.config.gala_url}"
-  end
-end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,6 +50,4 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
-  config.gala_url = "localhost:3333"
 end
