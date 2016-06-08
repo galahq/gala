8723f2788c87951e660a1e16c67d0a584c53e34d
Cameron Bothner <cameronbothner@gmail.com>
2016-06-08 18:00:47 -0400

    Fixes scrolling in edgenote modals.

--- app/components/ScrollLock.js
+++ app/components/ScrollLock.js
@@ -55,7 +55,7 @@ export var ScrollLock = ComposedComponent => class extends React.Component {
   }
 
   onScrollHandler(e) {
-    var elem = this.scrollElem;
+    var elem = ReactDOM.findDOMNode(this).querySelector(".Modal-contents")
     var scrollTop = elem.scrollTop;
     var scrollHeight = elem.scrollHeight;
     var height = elem.clientHeight;
