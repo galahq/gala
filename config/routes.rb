1d470d89022c4002968b6f3371f2a638f0a4cfb5
Cameron Bothner <cameronbothner@gmail.com>
2016-06-09 11:19:14 -0400

    Nest case activities, podcasts, and edgenotes under case route

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
   resources :groups
   resources :comment_threads
   resources :comments
-  resources :activities
-  resources :podcasts
-  resources :edgenotes
   devise_for :readers, controllers: {omniauth_callbacks:
                                      'readers/omniauth_callbacks'}
-  resources :cases, param: :slug
+  resources :cases, param: :slug do
+    resources :activities
+    resources :podcasts
+    resources :edgenotes
+  end
   root to: "cases#index"
 end
