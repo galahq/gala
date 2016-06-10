d0d4776dd69a3809fb5f63127a4b2e09239ae3ac
Cameron Bothner <cameronbothner@gmail.com>
2016-06-10 12:41:04 -0400

    Allow locale to be specified in the routes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,21 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
 
+  def default_url_options(options = {})
+    options[:locale] = params[:locale]
+    options
+  end
+
   private
 
   def set_locale
-    logger.debug "User preferred languages: #{http_accept_language.user_preferred_languages}"
-    I18n.locale = http_accept_language.compatible_language_from I18n.available_locales
-    logger.debug "Locale set to #{I18n.locale}"
+    if params[:locale]
+      I18n.locale = params[:locale]
+    else
+      logger.debug "User preferred languages: #{http_accept_language.user_preferred_languages}"
+      I18n.locale = http_accept_language.compatible_language_from I18n.available_locales
+      logger.debug "Locale set to #{I18n.locale}"
+    end
   end
 
   def authenticate_reader_from_token!
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
+locale_regex = /#{Rails.application.config.i18n.available_locales.map(&:to_s).join("|")}/
+
 Rails.application.routes.draw do
-  resources :enrollments
-  resources :groups
-  resources :comment_threads
-  resources :comments
   devise_for :readers, controllers: {omniauth_callbacks:
                                      'readers/omniauth_callbacks'}
-  resources :cases, param: :slug do
-    resources :activities
-    resources :podcasts
-    resources :edgenotes, param: :slug
+  scope "(:locale)", locale: locale_regex do
+    resources :enrollments
+    resources :groups
+    resources :comment_threads
+    resources :comments
+    resources :cases, param: :slug do
+      resources :activities
+      resources :podcasts
+      resources :edgenotes, param: :slug
+    end
   end
   root to: "cases#index"
 end
