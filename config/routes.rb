ab072f248e42d1ac32f5a5054722034b703539a5
Cameron Bothner <cameronbothner@gmail.com>
2016-06-10 15:27:21 -0400

    Shallow case route

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     resources :groups
     resources :comment_threads
     resources :comments
-    resources :cases, param: :slug do
+    resources :cases, param: :slug, shallow: true do
       resources :activities
       resources :podcasts
       resources :edgenotes, param: :slug
