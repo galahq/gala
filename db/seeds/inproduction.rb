cc401f77105c85e61889eda496781960bae8b5de
Pearl Zeng <zhuzeng@umich.edu>
2016-07-08 17:07:15 -0400

    Seeds the database with some in-production cases.

---
+++ db/seeds/inproduction.rb
@@ -0,0 +1,172 @@
+I18n.locale = :en
+
+  damremoval = Case.create(
+  slug: "maple-dam-removal",
+  published: false,
+  tags: %w(region:usa exercise:),
+  cover_url: "https://images.unsplash.com/photo-1465483967189-014cda7fe81a?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=34078177536e9d171de38c92181fb81b",
+  authors: [
+    "Paul Moore",
+    "Molly Watters",
+    "Kevin He",
+    "Lauren Edson",
+  ],
+
+  title: "Maple River–Lake Kathleen Dam Removal"
+  )
+
+  cookstove = Case.create(
+    slug: "gabon-cookstove",
+    published: false,
+    tags: %w(region:africa exercise:),
+    cover_url: "https://images.unsplash.com/photo-1462058294476-57b85c9f2bf5?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=b6f9d07443a5fb6998fa0fe95e548790",
+    authors: [
+      "Alex Clayton",
+      "Ember McCoy",
+      "Arun Agrawal",
+    ],
+
+    title: "Clean Cookstoves in Gabon: Finding a Sustainable Design"
+  )
+
+  landcover = Case.create(
+    slug: "ethiopia-landcover",
+    published: false,
+    tags: %w(region:africa exercise:),
+    cover_url: "https://images.unsplash.com/photo-1414236062502-90ed8c981d71?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=c1efac6c6687ed6f8667bc2736b9780b",
+    authors: [
+      "Rosie Pahl Donaldson",
+      "Alexander Rosen",
+      "Drew Philips",
+      "Larissa Larsen",
+    ],
+
+    title: "Land Cover Mapping of Addis Ababa, Ethiopia"
+  )
+
+  lead = Case.create(
+    slug: "lead",
+    published: false,
+    tags: %w(region:usa exercise:),
+    cover_url: "http://i2.wp.com/www.hotinhere.us/wp-content/uploads/2016/05/5150555940_1fcc375122_b-1.jpg?zoom=2&resize=562%2C311",
+    authors: [
+      "Mike Burbidge",
+      "Ember McCoy",
+      "Austin Martin",
+      "Paul Mohai",
+    ],
+
+    title: "The Dread of Lead in Gasoline: Environmental Decision-Making & Federal Legislation"
+  )
+
+  chinainafrica = Case.create(
+    slug: "china-africa-infrastructure",
+    published: false,
+    tags: %w(region:africa exercise:),
+    cover_url: "http://i1.wp.com/www.hotinhere.us/wp-content/uploads/2016/02/Arriver_de_chinois_dans_notre_societ%C3%A9.jpg?resize=1038%2C576",
+    authors: [
+      "Amanda Kaminski",
+      "Maddie Bianchie",
+      "Omolade Adunbi",
+    ],
+
+    title: "China–Africa: Infrastructure, Oil, and Sustainability"
+  )
+
+  ghanarenewable = Case.create(
+    slug: "ghana-renewable",
+    published: false,
+    tags: %w(region:africa exercise:quantitative),
+    cover_url: "https://images.unsplash.com/photo-1417144527634-653e3dec77b2?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=ac3484b17524e1b918a6fd7a6435193d",
+    authors: [
+      "Elizabeth Oliphant",
+      "Mark Finlay",
+      "Adam Simon",
+      "Brian Arbic",
+    ],
+
+    title: "Renewable Energy Infrastructure in Ghana"
+  )
+
+  smartmeters = Case.create(
+  slug: "maryland-smartmeters",
+  published: false,
+  tags: %w(region:usa exercise:),
+  cover_url: "https://images.unsplash.com/photo-1413882353314-73389f63b6fd?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=4d5df1dbe1d7b5a6bb6e92daf59b2e18",
+  authors: [
+    "Gianna Petito",
+    "Geoff Burmeister",
+    "Michael Moore",
+  ],
+
+  title: "Smarting over Smart Meters - The Heated Debate in Maryland over Real-Time Electric Consumption Data"
+  )
+
+  tesla = Case.create(
+    slug: "tesla-powerwall",
+    published: false,
+    tags: %w(region:usa exercise:),
+    cover_url: "https://images.unsplash.com/reserve/E5CwLOxQSFimIXJurfpq_IMG_6424%20(1).jpg?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=38642d551c3ba3b7401b27b1491a35b4",
+    authors: [
+      "Andrea Kraus",
+      "Braxton Mashburn",
+      "Jeremiah Johnson",
+    ],
+
+    title: "Green Mountain Power & Tesla Powerwall: Innovation within a conservative industry"
+  )
+
+  flintwater = Case.create(
+    slug: "flint-water",
+    published: false,
+    tags: %w(region:usa exercise:),
+    cover_url: "https://images.unsplash.com/14/unsplash_524000a90aaad_1.JPG?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=dc7275b6369c623d975484143fbb6a17",
+    authors: [
+      "Sarah Marshall",
+      "Kristin Steiner",
+      "Caitlin Harrington",
+      "Yajing Zhaou",
+      "Arun Agrawal",
+    ],
+
+    title: "The Flint Water Crisis: Funding Allocation in Nonprofit Collaboration"
+  )
+
+  gelman = Case.create(
+    slug: "gelman",
+    published: false,
+    tags: %w(region:usa exercise:),
+    cover_url: "http://media.mlive.com/annarbornews_impact/photo/19679893-standard.jpg",
+    authors: [
+      "Anna Prushinskaya",
+      "Allen Burton",
+    ],
+
+    title: "Gelman Plume Pollution"
+  )
+
+  cherrygrower = Case.create(
+    slug: "michigan-cherry-grower",
+    published: false,
+    tags: %w(region:usa exercise:),
+    cover_url: "https://images.unsplash.com/photo-1447690709975-318628b14c57?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=c1da61c882bdff4c01b8301c6aae96f0",
+    authors: [
+      "Edward Waisanen",
+      "Paige Fischer",
+    ],
+
+    title: "Northwest Michigan Cherry Growers' Adaptation to Climate Change"
+  )
+
+  fracking = Case.create(
+    slug: "michigan-fracking",
+    published: false,
+    tags: %w(region:usa exercise:quantitative),
+    cover_url: "http://i.huffpost.com/gen/3033338/images/o-FRACKING-facebook.jpg",
+    authors: [
+      "Alexander Truelove",
+      "Victoria Campbell-Arvai",
+    ],
+
+    title: "The wicked problem of unconventional oil and gas development in Michigan"
+  )
