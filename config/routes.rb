939eb38c9537918cdc83d6b3a9189ed3a88e2d3e
Cameron Bothner <cameronbothner@gmail.com>
2016-08-15 15:26:28 -0400

    Routes to deal with links that are already made public.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 locale_regex = /#{Rails.application.config.i18n.available_locales.map(&:to_s).join("|")}/
 
 Rails.application.routes.draw do
+  get '/read/1071/(*x)', to: redirect('/fr/cases/mi-wolves')
+  get '/read/862/(*x)', to: redirect('/cases/indonesia-conservation')
+  get '/read/611/(*x)', to: redirect('/cases/ethiopia-napa')
+  get '/read/497/(*x)', to: redirect('/cases/mi-wolves')
+
   scope "(:locale)", locale: locale_regex do
     resources :enrollments
     resources :groups
