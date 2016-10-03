b81f30708cc30e4dbb324de36a0a505213581a3f
Cameron Bothner <cameronbothner@gmail.com>
2016-10-03 17:20:30 -0400

    Allow X-Origin for Edgenote#show.

--- app/controllers/edgenotes_controller.rb
+++ app/controllers/edgenotes_controller.rb
@@ -1,6 +1,7 @@
 class EdgenotesController < ApplicationController
   before_action :set_edgenote, only: [:show, :update, :destroy]
   before_action :set_case, only: [:create]
+  before_action :set_cors_headers, only: [:show]
 
   # GET /edgenotes
   def index
@@ -51,5 +52,12 @@ class EdgenotesController < ApplicationController
     # Only allow a trusted parameter "white list" through.
     def edgenote_params
       params.require(:edgenote).permit(:caption, :format, :thumbnail_url, :content, :embed_code, :website_url, :image_url, :pdf_url, :instructions, :photo_credit, :slug)
+    end
+
+    def set_cors_headers
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+      headers['Access-Control-Request-Method'] = '*'
+      headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     end
 end
