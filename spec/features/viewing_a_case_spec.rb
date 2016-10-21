6f175fea72ce56a4ea088dadb4733f9d5ac41dc4
Cameron Bothner <cameronbothner@gmail.com>
2016-10-21 10:14:49 -0400

    Refactors viewing case spec, optimizing db writes.

--- spec/features/viewing_a_case_spec.rb
+++ spec/features/viewing_a_case_spec.rb
@@ -1,46 +1,59 @@
 require 'rails_helper'
 
 feature 'Viewing a case' do
-  let!(:published_case) { create :case, :featured, :published}
-  let!(:forthcoming_case) { create :case, :featured }
+  let(:user) { create :reader }
 
   context 'as a normal user' do
-    let(:user) { create :reader }
+    context 'a published case' do
+      let!(:published_case) { create :case, :featured, :published}
+      before { login_as user }
 
-    before { login_as user }
+      scenario 'is accessible' do
+        click_link published_case.title
+        expect(page).to have_content 'Table of Contents'
 
-    scenario 'published cases are accessible' do
-      click_link published_case.title
-      expect(page).to have_content 'Table of Contents'
+        click_link published_case.pages.first.title
+        expect(all('.Card').count).to eq published_case.pages.first.cards.count
 
-      click_link published_case.pages.first.title
-      expect(all('.Card').count).to eq published_case.pages.first.cards.count
-
-      click_link "Next:"
-      expect(all('.Card').count).to eq published_case.pages.second.cards.count
+        click_link "Next:"
+        expect(all('.Card').count).to eq published_case.pages.second.cards.count
+      end
     end
 
-    scenario 'forthcoming cases are not accessible' do
-      expect(page).to have_css '.catalog-case-unpublished-banner'
-      click_link forthcoming_case.title
-      expect(current_path).to eq root_path
+    context 'a forthcoming case' do
+      let!(:forthcoming_case) { create :case, :featured }
+      before { login_as user }
+
+      scenario 'is not accessible' do
+        expect(page).to have_css '.catalog-case-unpublished-banner'
+        click_link forthcoming_case.title
+        expect(current_path).to eq root_path
+      end
     end
+
   end
 
   context 'as a normal user enrolled in a forthcoming case' do
-    let(:enrollment) { create :enrollment, case: forthcoming_case }
-    let!(:another_forthcoming_case) { create :case }
+    let(:enrollment) { create :enrollment}
 
-    before { login_as enrollment.reader }
+    context 'that case' do
+      let(:forthcoming_case) { enrollment.case }
+      before { login_as enrollment.reader }
 
-    scenario 'that case is accessible' do
-      click_link forthcoming_case.title
-      expect(page).to have_content 'Table of Contents'
+      scenario 'is accessible' do
+        click_link forthcoming_case.title
+        expect(page).to have_content 'Table of Contents'
+      end
     end
 
-    scenario 'other forthcoming cases are also accessible' do
-      click_link another_forthcoming_case.title
-      expect(current_path).to eq root_path
+    context 'a different forthcoming case' do
+      let!(:another_forthcoming_case) { create :case }
+      before { login_as enrollment.reader }
+
+      scenario 'is not accessible' do
+        click_link another_forthcoming_case.title
+        expect(current_path).to eq root_path
+      end
     end
   end
 
