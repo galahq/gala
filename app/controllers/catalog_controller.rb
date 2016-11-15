cc1fb376672b1b8b92d8addafe84dc090d03502c
Cameron Bothner <cameronbothner@gmail.com>
2016-11-15 12:21:44 -0500

    Lets readers quickly access their favorite cases

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -3,8 +3,11 @@ class CatalogController < ApplicationController
     @cases = Case.where.not(cover_url: nil)
       .includes(:activities, :podcasts, :enrollments, :edgenotes, pages: [:cards])
       .sort_by &:kicker
-    @featured = @cases.select(&:featured?).sort.reverse
-    @index = @cases.select(&:in_index?).sort_by &:kicker
+    @my_cases = current_reader.cases.order(updated_at: :desc) if reader_signed_in?
+
+    cases_in_catalog = @cases - @my_cases rescue @cases
+    @featured = cases_in_catalog.select(&:featured?).sort.reverse
+    @index = cases_in_catalog.select(&:in_index?).sort_by &:kicker
     render layout: "window"
   end
 end
