24f1c1896ab170f065d09ffc3e8eea485f33dc51
Cameron Bothner <cameronbothner@gmail.com>
2016-12-09 10:58:51 -0500

    Redirect to login pages for unauth'd users

--- app/components/case.entry.js
+++ app/components/case.entry.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, IndexRoute, useRouterHistory } from 'react-router'
+import { Router, Route, IndexRoute, useRouterHistory, Redirect } from 'react-router'
 import { createHashHistory } from 'history'
 
 import Case from 'Case.js'
@@ -12,17 +12,25 @@ import EdgenoteContents from 'EdgenoteContents.js'
 
 const appHistory = useRouterHistory(createHashHistory)({ queryKey: false  })
 
+let authenticatedRoutes = [
+  <Route onEnter={() => window.scrollTo(0, 0)} path="edgenotes" component={EdgenoteGallery}>
+    <Route path=":edgenoteID" component={EdgenoteContents} />
+  </Route>,
+  <Route onEnter={() => window.scrollTo(0, 0)} path=":selectedPage" component={CaseReader}>
+    <Route path="edgenotes/:edgenoteID" component={EdgenoteContents} />
+  </Route>,
+  <Route path="podcasts/:podcastID" component={PodcastOverview} />
+]
+
 render((
   <Router history={appHistory}>
     <Route path="/(edit)" component={Case}>
       <IndexRoute component={CaseOverview} />
-      <Route onEnter={() => window.scrollTo(0, 0)} path="edgenotes" component={EdgenoteGallery}>
-        <Route path=":edgenoteID" component={EdgenoteContents} />
-      </Route>
-      <Route onEnter={() => window.scrollTo(0, 0)} path=":selectedPage" component={CaseReader}>
-        <Route path="edgenotes/:edgenoteID" component={EdgenoteContents} />
-      </Route>
-      <Route path="podcasts/:podcastID" component={PodcastOverview} />
+
+      { window.caseData.reader
+        ? authenticatedRoutes
+        : <Redirect from="*" to="/" /> }
+
     </Route>
   </Router>
 ), document.getElementById('container'))
