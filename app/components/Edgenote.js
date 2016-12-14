446a2a065ed293c820cb93f472dd72c475eb85a6
Cameron Bothner <cameronbothner@gmail.com>
2016-12-14 16:26:04 -0500

    Audio edgenotes in new design

--- app/components/Edgenote.js
+++ app/components/Edgenote.js
@@ -1,8 +1,34 @@
 import React from 'react'  // eslint-disable-line no-unused-vars
 import ImageZoom from 'react-medium-image-zoom'
 
+const backgroundedStyle = {
+  backgroundColor: "#49647D",
+  padding: '0.5em 1em',
+  borderRadius: '2px 2px 0 0'
+}
+
+const lightGreen = "#6ACB72"
+
+const highlightableStyle = {
+  transition: `background .15s cubic-bezier(.33, .66, .66, 1),
+    box-shadow .15s cubic-bezier(.33, .66, .66, 1),
+    color .15s cubic-bezier(.33, .66, .66, 1)`
+}
+const highlightedStyle = {
+  backgroundColor: lightGreen,
+  color: "#000",
+  boxShadow: `
+    ${lightGreen} -0.15em 0      0,
+    ${lightGreen} -0.15em 0.05em 0,
+    ${lightGreen} 0.2em   0.05em 0,
+    ${lightGreen} 0.2em   0      0`
+}
+
+
+
 export const Edgenote = ({contents, selected}) => {
-  let {slug, caption, youtubeSlug, pullQuote, imageUrl, callToAction, websiteUrl} = contents
+  let {slug, caption, youtubeSlug, pullQuote, imageUrl, callToAction,
+    websiteUrl, audioUrl, attribution} = contents
 
   let ConditionalLink = callToAction ? "a" : "div"
   let conditionalHoverCallbacks = callToAction
@@ -13,32 +39,26 @@ export const Edgenote = ({contents, selected}) => {
   return <figure className="edge" id={slug} {...conditionalHoverCallbacks}>
     <ConditionalLink target="_blank" href={websiteUrl}>
       <YouTube slug={youtubeSlug} />
-      { !!youtubeSlug || <PullQuote contents={pullQuote} selected={selected} /> }
       { !!youtubeSlug || !!pullQuote || <Image src={imageUrl} /> }
+
+      <Background visible={!!audioUrl}>
+        { !!youtubeSlug || <PullQuote contents={pullQuote} selected={selected} /> }
+        <Attribution name={attribution} />
+      </Background>
+      <AudioPlayer src={audioUrl} />
+
       <Caption contents={caption} selected={selected} />
       <CallToAction contents={callToAction} />
     </ConditionalLink>
   </figure>
 }
 
-
-
-const lightGreen = "#6ACB72"
-
-const canHighlight = {
-  transition: `background .15s cubic-bezier(.33, .66, .66, 1),
-    box-shadow .15s cubic-bezier(.33, .66, .66, 1),
-    color .15s cubic-bezier(.33, .66, .66, 1)`
-}
-const highlighted = {
-  backgroundColor: lightGreen,
-  color: "#000",
-  boxShadow: `
-    ${lightGreen} -0.15em 0      0,
-    ${lightGreen} -0.15em 0.05em 0,
-    ${lightGreen} 0.2em   0.05em 0,
-    ${lightGreen} 0.2em   0      0`
-}
+const Image = ({src}) => src
+  ? <ImageZoom
+      shouldRespectMaxDimension
+      defaultStyles={{overlay: {backgroundColor: '#1D2934'}}}
+      image={{style: {width: '100%', minHeight: '3em'}, src: src}} />
+  : null
 
 const YouTube = ({slug}) => slug
   ? <iframe
@@ -46,6 +66,33 @@ const YouTube = ({slug}) => slug
       src={`https://www.youtube.com/embed/${slug}` }
       frameBorder="0"
       allowFullScreen />
+  : null
+
+const AudioPlayer = ({src}) => src
+  ? <audio
+    style={{width: '100%', borderRadius: 2, borderBottom: `4px solid ${lightGreen}`}}
+    controls
+    preload
+    src={src} />
+  : null
+
+const Background = ({visible, children}) => <div style={visible ? backgroundedStyle : {}}>{children}</div>
+
+const CallToAction = ({contents}) => contents
+  ? <p style={{margin: "0.25em 0 0 0"}}>{contents}&nbsp;›</p>
+  : null
+
+
+const Caption = ({contents, selected}) => contents
+  ? <figcaption
+    style={{
+      margin: '0.25em 0 0 0',
+      fontSize: "110%",
+      lineHeight: 1.1,
+      display: "inline",
+      ...highlightableStyle,
+      ...(selected && highlightedStyle)
+    }}>{contents}</figcaption>
   : null
 
 const PullQuote = ({contents, selected}) => contents
@@ -56,32 +103,12 @@ const PullQuote = ({contents, selected}) => contents
       margin: '0 0 0.5em 0',
       padding: '0',
       display: "inline",
-      ...canHighlight,
-      ...(selected && highlighted)
+      ...highlightableStyle,
+      ...(selected && highlightedStyle)
     }}
-      children={`“${contents}”`} />
+      children={`“${contents}…”`} />
   : null
 
-const Image = ({src}) => src
-  ? <ImageZoom
-      shouldRespectMaxDimension
-      defaultStyles={{overlay: {backgroundColor: '#1D2934'}}}
-      image={{style: {width: '100%', minHeight: '3em'}, src: src}} />
+const Attribution = ({name}) => name
+  ? <cite style={{textAlign: 'right', display: 'block', fontStyle: 'normal', margin: '0.5em 0 0.25em 0'}}>— {name}</cite>
   : null
-
-const Caption = ({contents, selected}) => contents
-  ? <figcaption
-    style={{
-      margin: '0.25em 0 0 0',
-      fontSize: "110%",
-      lineHeight: 1.1,
-      display: "inline",
-      ...canHighlight,
-      ...(selected && highlighted)
-    }}>{contents}</figcaption>
-  : null
-
-const CallToAction = ({contents}) => contents
-  ? <p style={{margin: "0.25em 0 0 0"}}>{contents}&nbsp;›</p>
-  : null
-
