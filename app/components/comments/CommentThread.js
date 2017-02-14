0e01d66da95ee1b46555698a943515aac1ac4060
Cameron Bothner <cameronbothner@gmail.com>
2017-02-14 12:43:37 -0500

    Show truncated responses to an initial comment in list

--- app/components/comments/CommentThread.js
+++ app/components/comments/CommentThread.js
@@ -5,27 +5,54 @@ import Truncate from 'react-truncate'
 
 function mapStateToProps(state, ownProps) {
   const thread = state.commentThreadsById[ownProps.threadId]
-  const firstCommentId = thread.commentIds[0]
-  const firstComment = firstCommentId ? state.commentsById[firstCommentId] : {}
+  const comments = thread.commentIds.map( x => state.commentsById[x] )
+  const firstComment = comments.length > 0 ? comments[0] : {}
 
   return {
     selected: ownProps.threadId === state.ui.selectedCommentThread,
-    author: firstComment.reader
-      ? firstComment.reader.name
-      : state.caseData.reader.name,
-    snippet: firstComment.content || "New comment...",
+    lead: {
+      author: firstComment.reader
+        ? firstComment.reader.name
+        : state.caseData.reader.name,
+      content: firstComment.content || "New comment...",
+    },
+    responses: comments.splice(1),
   }
 }
 
-const CommentThread = ({author, snippet, threadId, selected, selectCommentThread,
+const CommentThread = ({lead, responses, threadId, selected, selectCommentThread,
                        last}) =>
   <li key={threadId} style={{
     ...styles.commentListItem,
     borderBottom: last || '1px solid #513992',
     ...(selected ? {backgroundColor: "#493092"} : {}),
   }} onClick={() => selectCommentThread(threadId)}>
-  <h4 style={styles.author}>{author}</h4>
-  <p style={styles.commentSnippet}><Truncate lines={3}>{snippet}</Truncate></p>
+  <h4 style={styles.author}>{lead.author}</h4>
+  <p style={styles.commentSnippet}>
+    <Truncate lines={3}>{lead.content}</Truncate>
+  </p>
+
+  {
+    responses.map((r, i) => {
+      const numOthers = responses.length - 2
+      switch(i) {
+        case 0:
+        case 1:
+          return <p
+            key={i}
+            style={{...styles.commentSnippet, ...styles.oneLineSnippet}}
+          >
+            <span style={styles.initials}>{r.reader.initials}:</span>
+            <span>{r.content}</span>
+          </p>
+        case 2:
+          return <p key="2" style={styles.commentSnippet}>
+            {numOthers} other response{numOthers === 1 ? '' : 's'}
+          </p>
+        default: return null
+      }
+    })
+  }
 </li>
 
 export default connect(
@@ -55,5 +82,16 @@ const styles = {
     margin: '0 0 0 1em',
     fontWeight: 400,
     lineHeight: 1.4,
+  },
+
+  initials: {
+    fontWeight: 600,
+    marginRight: '0.5em',
+  },
+
+  oneLineSnippet: {
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    whiteSpace: 'nowrap',
   },
 }
