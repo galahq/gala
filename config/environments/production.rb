d52930f07402612e879b7a4303d732de50520a9d
Cameron Bothner <cameronbothner@gmail.com>
2017-03-07 14:41:40 -0500

    Hotfix for ActionCable allowed request origins

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,8 +35,8 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Action Cable endpoint configuration
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://www.learnmsc.org/cable'
+  config.action_cable.allowed_request_origins = [ 'http://www.learnmsc.org', 'https://www.learnmsc.org' ]
 
   # Don't mount Action Cable in the main server process.
   # config.action_cable.mount_path = nil
