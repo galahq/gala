21d3f0598670eb27143c4a40484cf3d9d93dce1f
Cameron Bothner <cameronbothner@gmail.com>
2017-04-06 13:36:06 -0400

    Specify the first page path without caching the page model

--- spec/features/tracking_a_reader_spec.rb
+++ spec/features/tracking_a_reader_spec.rb
@@ -9,14 +9,12 @@ feature 'Tracking a reader' do
     before { login_as reader }
 
     scenario 'records when they view a card' do
-      first_page_path = "#{case_path 'en', kase}/#{kase.pages.first.case_element.position}"
-      visit first_page_path
+      visit "#{case_path 'en', kase}/1"
       sleep 1
       visit root_path
       expect(kase.pages.first.cards.first.views).to eq 1
       expect(kase.pages.first.cards.first.uniques).to eq 1
-
-      visit first_page_path
+      visit "#{case_path 'en', kase}/1"
       sleep 1
       visit root_path
       expect(kase.pages.first.cards.first.views).to eq 2
@@ -30,8 +28,7 @@ feature 'Tracking a reader' do
     before { login_as reader }
 
     scenario 'does not record when they view a card' do
-      first_page_path = "#{case_path 'en', kase}/#{kase.pages.first.case_element.position}"
-      visit first_page_path
+      visit "#{case_path 'en', kase}/1"
       sleep 1
       visit root_path
       expect(kase.pages.first.cards.first.views).to eq 0
