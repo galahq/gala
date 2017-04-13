528ab992f086b0b1207b6ba7b309e538fd71c1bf
Pearl Zeng <zhuzeng@umich.edu>
2017-04-13 17:59:40 -0400

    Fixes case dashboard flex and float.

--- app/javascript/dashboard/Dashboard.jsx
+++ app/javascript/dashboard/Dashboard.jsx
@@ -34,27 +34,44 @@ export class Dashboard extends React.Component {
   }
 
   render () {
-    let { initials } = this.props
-    let { editing } = this.state
+    const { initials } = this.props
+    const { editing } = this.state
+    const hasCases = this.state.cases.length > 0
+
     return (
-      <div className="catalog-dashboard">
+      <div
+        className={
+          `catalog-dashboard ${hasCases ? '' : 'catalog-dashboard--empty'}`
+        }
+      >
+
+        {!hasCases &&
+          <div className="catalog-dashboard__my-cases--empty__case">
+            <div>
+              <img
+                src="https://images.unsplash.com/photo-1429704658776-3d38c9990511?ixlib=rb-0.3.5&amp;w=300&amp;h=300&amp;fit=crop&amp;crop=focalpoint&amp;fp-x=.535&amp;fp-y=.6&amp;fp-z=2.5&amp;mono=493092"
+              />
+              <strong><I18n meaning="study_a_case" /></strong>
+            </div>
+          </div>}
 
         <div className="catalog-dashboard__reader">
           <h1>Hello, {initials}</h1>
-          <h2>
-            <I18n meaning="my_cases" />
-            <a
-              className="catalog-dashboard__edit"
-              onClick={this.handleSetEditing}
-            >
-              {editing ? 'Done' : 'Edit'}
-              <span
-                dangerouslySetInnerHTML={{
-                  __html: require(`images/dashboard-${editing ? 'done' : 'edit'}.svg`),
-                }}
-              />
-            </a>
-          </h2>
+          {hasCases &&
+            <h2>
+              <I18n meaning="my_cases" /><br />
+              <a
+                className="catalog-dashboard__edit"
+                onClick={this.handleSetEditing}
+              >
+                {editing ? 'Done' : 'Edit'}
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: require(`images/dashboard-${editing ? 'done' : 'edit'}.svg`),
+                  }}
+                />
+              </a>
+            </h2>}
         </div>
 
         {this.state.cases.length > 0
@@ -65,33 +82,24 @@ export class Dashboard extends React.Component {
                 editing={this.state.editing}
                 deleteEnrollmentFor={this.deleteEnrollmentFor}
                 case={kase}
-                />
+              />
               ))}
           </div>
-          : <div className="catalog-dashboard__my-cases--empty">
-            <div className="catalog-dashboard__my-cases--empty__case">
-              <img
-                src="https://images.unsplash.com/photo-1429704658776-3d38c9990511?ixlib=rb-0.3.5&amp;w=300&amp;h=300&amp;fit=crop&amp;crop=focalpoint&amp;fp-x=.535&amp;fp-y=.6&amp;fp-z=2.5&amp;mono=493092"
-                />
-              <strong><I18n meaning="study_a_case" /></strong>
-            </div>
+          : <div className="catalog-dashboard__my-cases--empty__intro">
+            <h2>Choose for yourself</h2>
 
-            <div className="catalog-dashboard__my-cases--empty__intro">
-              <h3>Choose for yourself</h3>
+            <ul>
+              <li>
+                  Meet different stakeholders and dive deep with a multimodal narrative.
+                </li>
+              <li>
+                  Shortcut experience by putting principles into practice.
+                </li>
+            </ul>
 
-              <ul>
-                <li>
-                    Meet different stakeholders and dive deep with a multimodal narrative.
-                  </li>
-                <li>
-                    Shortcut experience by putting principles into practice.
-                  </li>
-              </ul>
-
-              <p>
-                  Cases you select from the index below will be presented here for easy access.
-                </p>
-            </div>
+            <p>
+                Cases you select from the index below will be presented here for easy access.
+              </p>
           </div>}
 
       </div>
