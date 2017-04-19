5f4e1c5538b47eac8a3f3f17f7deaf6c0105c324
Cameron Bothner <cameronbothner@gmail.com>
2017-04-19 15:24:28 -0400

    Get LTI key and secret from the environment

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -243,7 +243,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   config.omniauth :facebook, ENV["FACEBOOK_CLIENT_ID"], ENV["FACEBOOK_CLIENT_SECRET"]
   config.omniauth :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {name: "google"}
-  config.omniauth :lti, oauth_credentials: {test: "secret"}
+  config.omniauth :lti, oauth_credentials: Hash[ENV["LTI_KEY"], ENV["LTI_SECRET"]]
 
   if Rails.env.test?
     OmniAuth.config.test_mode = true
