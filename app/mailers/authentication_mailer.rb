654bf592abd92379f2428bf67276a5777f6f4bb0
Cameron Bothner <cameronbothner@gmail.com>
2017-04-21 17:07:16 -0400

    Makes devise send plain text emails as well as HTML

---
+++ app/mailers/authentication_mailer.rb
@@ -0,0 +1,9 @@
+class AuthenticationMailer < Devise::Mailer
+  def devise_mail(record, action, opts={})
+    initialize_from_record(record)
+    mail(headers_for(action, opts)) do |format|
+      format.text
+      format.html
+    end
+  end
+end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -15,7 +15,7 @@ Devise.setup do |config|
   config.mailer_sender = 'msc-contact@umich.edu'
 
   # Configure the class responsible to send e-mails.
-  # config.mailer = 'Devise::Mailer'
+  config.mailer = 'AuthenticationMailer'
 
   # Configure the parent class responsible to send e-mails.
   config.parent_mailer = 'ApplicationMailer'
