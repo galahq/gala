7486933360892d14d7e2498d70e49c427f5a2ada
Cameron Bothner <cameronbothner@gmail.com>
2017-05-26 17:52:32 -0400

    Fixes deployment require statement

--- app/javascript/packs/deployment.entry.jsx
+++ app/javascript/packs/deployment.entry.jsx
@@ -6,7 +6,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
 
-import Deployment from 'Deployment'
+import Deployment from 'deployment'
 
 import { FocusStyleManager } from '@blueprintjs/core'
 FocusStyleManager.onlyShowFocusOnTabs()
