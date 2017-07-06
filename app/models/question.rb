09675b90fc6b1481548dc62bd4eea775f58743c6
Cameron Bothner <cameronbothner@gmail.com>
2017-07-06 17:52:19 -0400

    Removes the last remaining trace of _i18n suffixes

--- app/models/question.rb
+++ app/models/question.rb
@@ -7,7 +7,7 @@ class Question < ApplicationRecord
   include Mobility
   translates :content, fallbacks: true
 
-  validates :content_i18n, presence: true
+  validates :content, presence: true
   validates :correct_answer, inclusion: { in: ->(question) { question.options } },
                              if: ->(question) { !question.options.empty? }
 
