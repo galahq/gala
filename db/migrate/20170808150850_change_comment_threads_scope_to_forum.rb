c6c8903540ef1a6545e2144daa872f019888c88f
Cameron Bothner <cameronbothner@gmail.com>
2017-08-18 10:46:19 -0400

    Changes migration to move extant comment_threads to case’s global forum

--- db/migrate/20170808150850_change_comment_threads_scope_to_forum.rb
+++ db/migrate/20170808150850_change_comment_threads_scope_to_forum.rb
@@ -3,6 +3,21 @@
 class ChangeCommentThreadsScopeToForum < ActiveRecord::Migration[5.0]
   def change
     add_reference :comment_threads, :forum, foreign_key: true
+
+    reversible do |dir|
+      dir.up do
+        Case.find_each do |c|
+          forum = Forum.create(case: c, community: nil)
+
+          CommentThread.joins(:card)
+                       .where(cards: { case_id: c.id },
+                              group_id: nil)
+                       .update_all forum_id: forum.id
+        end
+      end
+      dir.down {}
+    end
+
     remove_reference :comment_threads, :group, foreign_key: true
   end
 end
