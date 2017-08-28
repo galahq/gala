d9e2260862bb82a9cbd76fa05ce59efc30588942
Cameron Bothner <cameronbothner@gmail.com>
2017-08-28 13:27:40 -0400

    Creates communities for each group and forums for each deployment

--- db/migrate/20170808143606_create_forums.rb
+++ db/migrate/20170808143606_create_forums.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateForums < ActiveRecord::Migration[5.0]
   def change
     create_table :forums do |t|
@@ -5,6 +7,18 @@ class CreateForums < ActiveRecord::Migration[5.0]
       t.references :community, foreign_key: true
 
       t.timestamps
+    end
+
+    reversible do |dir|
+      dir.up do
+        Group.find_each do |g|
+          g.create_community name: g.name
+          g.deployments.each do |d|
+            g.community.forums.create case: d.case
+          end
+        end
+      end
+      dir.down {}
     end
   end
 end
