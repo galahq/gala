1c9861a227196e3d581befd308aa0d66567f92c2
Cameron Bothner <cbothner@umich.edu>
2017-08-30 17:06:58 -0400

    Refactors status bar to be way more readable (#88)

--- app/javascript/overview/StatusBar.jsx
+++ app/javascript/overview/StatusBar.jsx
@@ -1,5 +1,12 @@
+/**
+ * @providesModule StatusBar
+ * @flow
+ */
+
 import React from 'react'
 import { connect } from 'react-redux'
+import styled from 'styled-components'
+
 import { FormattedMessage } from 'react-intl'
 import { toggleEditing, saveChanges } from 'redux/actions'
 
@@ -13,51 +20,82 @@ function mapStateToProps (state: State) {
     editable: edit.possible,
     editing: inProgress,
     edited: edit.changed,
-    messages: {
-      instructions: inProgress
-        ? 'statusBar.editInstructions'
-        : published ? '' : 'statusBar.betaNotification',
-      editToggle: inProgress ? 'statusBar.endEdit' : 'statusBar.beginEdit',
-      save: 'statusBar.save',
-    },
+    published,
   }
 }
+
+type BarElement = { message: string, onClick?: () => any }
 
 function StatusBar ({
   editable,
   editing,
   edited,
-  messages,
+  published,
   toggleEditing,
   saveChanges,
 }) {
+  const elements: Array<?BarElement> = [
+    // Instructions
+    editing
+      ? { message: 'editInstructions' }
+      : !published ? { message: 'betaNotification' } : null,
+
+    // Edit toggle
+    editable || editing
+      ? { message: editing ? 'endEdit' : 'beginEdit', onClick: toggleEditing }
+      : null,
+
+    // Save changes
+    edited ? { message: 'save', onClick: saveChanges } : null,
+  ]
+
+  if (!elements.some(x => x)) return null
+
+  const StatusBarElement = ({ message, onClick }: BarElement) => {
+    const Tag = onClick ? 'a' : 'span'
+    return (
+      <Tag onClick={onClick}>
+        <FormattedMessage id={`statusBar.${message}`} />
+      </Tag>
+    )
+  }
+
   return (
-    <div
-      className={
-        (messages.instructions || editable) &&
-        `flash flash-${editing ? 'editing' : 'info'}`
-      }
-    >
-      {messages.instructions && <FormattedMessage id={messages.instructions} />}
-      {(editable || edited) &&
-        <span>
-          {messages.instructions && <span> — </span>}
-          {editable &&
-            <a onClick={toggleEditing}>
-              {messages.editToggle &&
-                <FormattedMessage id={messages.editToggle} />}
-            </a>}
-          {edited && [
-            <span key="1"> </span>,
-            <a key="2" onClick={saveChanges}>
-              {messages.save && <FormattedMessage id={messages.save} />}
-            </a>,
-          ]}
-        </span>}
-    </div>
+    <Bar editing={editing}>
+      {elements
+        .reduce((array, element) => {
+          if (element == null) return array
+          array.push(<StatusBarElement key={element.message} {...element} />)
+          array.push(<span key={`${element.message}:after`}> — </span>)
+          return array
+        }, [])
+        .slice(0, -1)}
+    </Bar>
   )
 }
 
 export default connect(mapStateToProps, { toggleEditing, saveChanges })(
   StatusBar
 )
+
+const Bar = styled.div`
+  width: 100%;
+  padding: 0.2em;
+
+  color: ${({ editing }) => (editing ? '#262626' : '#ebeae4')};
+  background-color: ${({ editing }) => (editing ? '#EBEAE4' : '#1d3f5e')};
+  border-bottom: 2px solid ${({ editing }) =>
+      editing ? '#c0bca9' : '#193c5b'};
+
+  font: 90% 'tenso';
+  text-transform: uppercase;
+  text-align: center;
+  letter-spacing: 0.05em;
+  text-transform: initial;
+  letter-spacing: 0em;
+
+  & a {
+    color: inherit;
+    text-decoration: underline;
+  }
+`
