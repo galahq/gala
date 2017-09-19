e1626ff5db2d03d33efd061d66d2f57c6845bc74
Cameron Bothner <cameronbothner@gmail.com>
2017-09-19 14:52:55 -0400

    Tries out common chunks

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack')
 const { environment } = require('@rails/webpacker')
 
 environment.loaders.delete('file')
@@ -5,5 +6,24 @@ environment.loaders.set('svg', {
   test: /\.svg$/,
   loader: 'raw-loader',
 })
+
+environment.plugins.set(
+  'CommonsChunkVendor',
+  new webpack.optimize.CommonsChunkPlugin({
+    name: 'vendor',
+    minChunks: module => {
+      // this assumes your vendor imports exist in the node_modules directory
+      return module.context && module.context.indexOf('node_modules') !== -1
+    },
+  })
+)
+
+environment.plugins.set(
+  'CommonsChunkManifest',
+  new webpack.optimize.CommonsChunkPlugin({
+    name: 'manifest',
+    minChunks: Infinity,
+  })
+)
 
 module.exports = environment
