63985acf5d2f6f9cb7ef8e4e8e271ab2f27bed2d
Cameron Bothner <cameronbothner@gmail.com>
2017-10-23 16:34:10 -0400

    Adds a library model to the db

---
+++ spec/factories/libraries.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :library do
+    sequence(:slug) { |n| "michigan-sustainaility-cases-#{n}"}
+    name "Michigan Sustainability Cases"
+    logo_url "https://msc-gala.imgix.net/block-m.svg"
+    background_color "#00274c"
+    foreground_color "#ffcb05"
+  end
+end
