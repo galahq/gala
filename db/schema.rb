6c960fd470c78039b495cd3bca79a681c917b4b7
Cameron Bothner <cameronbothner@gmail.com>
2017-10-25 16:53:34 -0400

    Adds translatable acknowledgements field to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024180715) do
+ActiveRecord::Schema.define(version: 20171025205053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -115,6 +115,7 @@ ActiveRecord::Schema.define(version: 20171024180715) do
     t.float "longitude"
     t.float "zoom"
     t.bigint "library_id"
+    t.jsonb "acknowledgements"
     t.index ["library_id"], name: "index_cases_on_library_id"
     t.index ["slug"], name: "index_cases_on_slug", unique: true
     t.index ["tags"], name: "index_cases_on_tags", using: :gin
