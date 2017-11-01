46e4e3bbc047a0f437091c7871881bf3b0a9adfc
Cameron Bothner <cameronbothner@gmail.com>
2017-11-01 14:21:08 -0400

    Adds a search bar to the catalog toolbar

--- app/javascript/catalog/CatalogToolbar.jsx
+++ app/javascript/catalog/CatalogToolbar.jsx
@@ -3,12 +3,16 @@
  * @flow
  */
 
-import React from 'react'
-
+import * as React from 'react'
+import { injectIntl } from 'react-intl'
 import { withRouter } from 'react-router-dom'
 
-import Toolbar from 'utility/Toolbar'
+import { InputGroup } from '@blueprintjs/core'
 
+import Toolbar from 'utility/Toolbar'
+import { getSearchPath } from 'catalog/SearchForm'
+
+import type { IntlShape } from 'react-intl'
 import type { ContextRouter } from 'react-router-dom'
 
 const CatalogToolbar = ({ history }: ContextRouter) => (
@@ -28,9 +32,49 @@ const CatalogToolbar = ({ history }: ContextRouter) => (
         },
       ],
       [],
-      [],
+      [{ component: <Search /> }],
     ]}
   />
 )
 
 export default withRouter(CatalogToolbar)
+
+class SearchField extends React.Component<ContextRouter & { intl: IntlShape }> {
+  input: ?HTMLInputElement
+
+  handleSubmit = (e: SyntheticEvent<*>) => {
+    e.preventDefault()
+    this.input &&
+      this.props.history.push(
+        getSearchPath({
+          q: this.input.value,
+        })
+      )
+    this.input && this.input.blur()
+    this.input && (this.input.value = '')
+  }
+
+  render () {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <InputGroup
+          inputRef={el => (this.input = el)}
+          className="pt-round"
+          leftIconName="search"
+          rightElement={
+            <button
+              className="pt-button pt-minimal pt-intent-success pt-icon-arrow-right"
+              onClick={this.handleSubmit}
+            />
+          }
+          placeholder={this.props.intl.formatMessage({
+            id: 'catalog.search',
+            defaultMessage: 'Search cases',
+          })}
+        />
+      </form>
+    )
+  }
+}
+
+const Search = withRouter(injectIntl(SearchField))
