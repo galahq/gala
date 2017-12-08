167a69e178f0048a5d8c6f82049398d314a63aa1
Cameron Bothner <cameronbothner@gmail.com>
2017-12-08 13:35:43 -0500

    Adds an ErrorBoundary component around the whole app

---
+++ app/javascript/utility/ErrorBoundary.jsx
@@ -0,0 +1,68 @@
+/**
+ * @providesModule ErrorBoundary
+ * @flow
+ */
+
+import * as React from 'react'
+import styled from 'styled-components'
+
+import { NonIdealState } from '@blueprintjs/core'
+
+class ErrorBoundary extends React.Component<
+  { children: React.Node },
+  { hasError: boolean, error: ?Error, info: ?{ componentStack: string } }
+> {
+  state = { hasError: false, error: null, info: null }
+
+  componentDidCatch (error: Error, info: string) {
+    this.setState({ hasError: true, error, info })
+  }
+
+  render () {
+    const { hasError, error, info } = this.state
+    if (hasError) {
+      // You can render any custom fallback UI
+      return (
+        <Container>
+          <NonIdealState
+            visual="error"
+            title="Something went wrong"
+            description={
+              <InfoBox>
+                <code>
+                  Uncaught {error && error.name}: {error && error.message}
+                  {'\n'}
+                  {info && info.componentStack}
+                </code>
+              </InfoBox>
+            }
+          />
+        </Container>
+      )
+    }
+    return this.props.children
+  }
+}
+export default ErrorBoundary
+
+const Container = styled.div.attrs({ className: 'pt-dark' })`
+  margin-top: 40px;
+
+  & .pt-non-ideal-state {
+    max-width: 800px;
+
+    & .pt-non-ideal-state-description {
+      width: 100%;
+    }
+  }
+`
+
+const InfoBox = styled.pre`
+  text-align: left;
+  margin: 16px;
+  overflow-x: scroll;
+
+  & code {
+    white-space: pre;
+  }
+`
--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,3 +1,9 @@
+const merge = require('webpack-merge')
 const environment = require('./environment')
 
-module.exports = environment.toWebpackConfig()
+// While webpacker still defaults to 'cheap-eval-source-map', we need to
+// override it because eval is seen as cross-origin, which keeps the errors
+// from surfacing
+const cheapModuleSourceMap = { devtool: 'cheap-module-source-map' }
+
+module.exports = merge(environment.toWebpackConfig(), cheapModuleSourceMap)
