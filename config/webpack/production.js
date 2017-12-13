828265ea6218017936d1e8258c588b51891a3cfe
Cameron Bothner <cameronbothner@gmail.com>
2017-12-13 09:43:21 -0500

    Adds a sourcemap in production

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -4,4 +4,7 @@ const environment = require('./environment')
 // mapbox-gl can’t be uglified for now, so we won’t...
 const fixMapbox = { module: { noParse: /(mapbox-gl)\.js$/ }}
 
-module.exports = merge(environment.toWebpackConfig(), fixMapbox)
+// Our code is open source anyway and I hate debugging otherwise
+const sourceMap = { devtool: 'source-map' }
+
+module.exports = merge(environment.toWebpackConfig(), fixMapbox, sourceMap)
