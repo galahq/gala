556f7a3c86352cec4cbbb9a6dcb34b1dc53c5c80
Cameron Bothner <cameronbothner@gmail.com>
2017-12-20 15:27:51 -0500

    feat: show authors’s institutions in tooltips

--- app/javascript/overview/AuthorsList.jsx
+++ app/javascript/overview/AuthorsList.jsx
@@ -4,13 +4,18 @@
  */
 
 import * as React from 'react'
+import styled from 'styled-components'
 
 import { acceptKeyboardClick } from 'shared/keyboard'
+
+import { Intent, Position, Tooltip } from '@blueprintjs/core'
+
 import AuthorsListForm from './AuthorsListForm'
 import Acknowledgements from './Acknowledgements'
 import { FormattedList } from 'shared/react-intl'
+import { LabelForScreenReaders } from 'utility/A11y'
 
-import type { Byline } from 'redux/state'
+import type { Author, Byline } from 'redux/state'
 import type { AuthorsListFormState } from './AuthorsListForm'
 
 class AuthorsList extends React.Component<
@@ -49,7 +54,9 @@ class AuthorsList extends React.Component<
       >
         <p>
           <FormattedList
-            list={authors.map(a => <span key={a.name}>{a.name}</span>)}
+            list={authors.map(a => (
+              <AuthorName key={a.name} author={a} canEdit={canEdit} />
+            ))}
           />
           <Acknowledgements contents={acknowledgements} />
           <br />
@@ -74,3 +81,34 @@ class AuthorsList extends React.Component<
 }
 
 export default AuthorsList
+
+const AuthorName = ({
+  author,
+  canEdit,
+}: {
+  author: Author,
+  canEdit: boolean,
+}) => {
+  const { name, institution } = author
+  return institution ? (
+    <StyledTooltip
+      content={institution}
+      isDisabled={canEdit}
+      intent={Intent.SUCCESS}
+      position={Position.BOTTOM_LEFT}
+    >
+      <span>
+        {name}
+        <LabelForScreenReaders>({institution})</LabelForScreenReaders>
+      </span>
+    </StyledTooltip>
+  ) : (
+    name
+  )
+}
+
+const StyledTooltip = styled(Tooltip).attrs({
+  className: 'pt-tooltip-indicator',
+})`
+  border-bottom-color: #ffffffaa;
+`
