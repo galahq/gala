82d315e9ebe48ca3426ef1d61b791b34d506ef13
Cameron Bothner <cameronbothner@gmail.com>
2018-01-09 11:49:38 -0500

    docs: document helpers

--- app/helpers/comment_threads_helper.rb
+++ app/helpers/comment_threads_helper.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# @see CommentThread
 module CommentThreadsHelper
   def threads_for_card(comment_threads, card_id)
     @comment_threads_by_card ||= comment_threads.group_by(&:card_id)
