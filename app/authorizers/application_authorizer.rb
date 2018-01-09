39dcce2398e30c498945528810303d893e1cb39e
Cameron Bothner <cameronbothner@gmail.com>
2018-01-09 12:04:19 -0500

    docs: document authorizers

--- app/authorizers/application_authorizer.rb
+++ app/authorizers/application_authorizer.rb
@@ -1,16 +1,18 @@
 # frozen_string_literal: true
 
 # Other authorizers should subclass this one
+# @abstract
 class ApplicationAuthorizer < Authority::Authorizer
   # Any class method from Authority::Authorizer that isn't overridden
   # will call its authorizer's default method.
   #
-  # @param [Symbol] adjective; example: `:creatable`
-  # @param [Object] reader - whatever represents the current user in your app
+  # @param _adjective [Symbol] example: `:creatable`
+  # @param reader [Object] whatever represents the current user in your app
   # @return [Boolean]
   def self.default(_adjective, reader)
     # 'Whitelist' strategy for security: anything not explicitly allowed is
     # considered forbidden.
+    # Editors can do anything.
     reader.has_cached_role? :editor
   end
 end
--- app/authorizers/deployment_authorizer.rb
+++ app/authorizers/deployment_authorizer.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
+# @see Deployment
 class DeploymentAuthorizer < ApplicationAuthorizer
+  # Instructors and editors are the only ones who can edit deployments.
+  # Since an LTI ContentItemsSelection request creates a new deployment, even
+  # a yet-unauthenticated reader from that source can edit that deployment
   def updatable_by?(reader, selection_params: nil)
     return true if reader.has_role? :editor
 
