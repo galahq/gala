95dd081d1689075169a3afac3d32bbb9bcd16efb
Cameron Bothner <cameronbothner@gmail.com>
2018-01-09 11:54:05 -0500

    docs: document channels

--- app/channels/reader_notifications_channel.rb
+++ app/channels/reader_notifications_channel.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Broadcasts notifications when other readers reply to a reader’s {Comment}
 class ReaderNotificationsChannel < ApplicationCable::Channel
   def subscribed
     stream_for current_reader
