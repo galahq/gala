e017297f23c3696845e9c6a7603430754861f963
Cameron Bothner <cameronbothner@gmail.com>
2018-01-09 12:04:19 -0500

    docs: document authorizers

--- app/authorizers/comment_thread_authorizer.rb
+++ app/authorizers/comment_thread_authorizer.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
+# @see CommentThread
 class CommentThreadAuthorizer < ApplicationAuthorizer
+  # A reader can only delete her own comment thread, unless she’s an editor
   def deletable_by?(reader)
     resource.reader == reader || super
   end
