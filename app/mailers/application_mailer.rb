82dbdf2d5deba2b43cb83b812a856ba830f82fbf
Cameron Bothner <cameronbothner@gmail.com>
2018-01-09 12:10:18 -0500

    doc: document mailers

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# @abstract
 class ApplicationMailer < ActionMailer::Base
   helper :application
 
--- app/mailers/authentication_mailer.rb
+++ app/mailers/authentication_mailer.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Customizations to the emails Devise sends
 class AuthenticationMailer < Devise::Mailer
   def devise_mail(record, action, opts = {})
     initialize_from_record(record)
