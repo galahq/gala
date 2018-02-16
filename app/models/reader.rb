2a0af751c00a4346c629cb2d93f6011f12acb156
Cameron Bothner <cameronbothner@gmail.com>
2018-02-16 10:27:21 -0500

    refactor: remove authority

--- app/models/reader.rb
+++ app/models/reader.rb
@@ -20,8 +20,6 @@
 #
 # @see AnonymousUser AnonymousUser: this model’s null object
 class Reader < ApplicationRecord
-  include Authority::UserAbilities
-
   default_scope { order(:name) }
 
   has_many :authentication_strategies, dependent: :destroy
