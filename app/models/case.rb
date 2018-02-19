e55f65b98b36acca78dcd56dedd1090ddc2ae7a6
Cameron Bothner <cameronbothner@gmail.com>
2018-02-19 12:00:12 -0500

    refactor: manage case slug with friendly_id

--- app/models/case.rb
+++ app/models/case.rb
@@ -3,7 +3,7 @@
 # A case study in Gala with attributes for the case’s “metadata” (the catalog or
 # overview information) and associations for all the case’s constituent parts.
 #
-# @attr slug [String] a globally unique kabob-case identifier: the URL param
+# @attr slug [String] the URL param (managed by friendly_id)
 # @attr kicker [Translated<String>] a two or three word tagline for the case.
 #   This comes from newspapers: the little mini-headline appearing above the hed
 #   to which continuations of the article refer (e.g. “from CORRUPTION, A1”)
@@ -30,6 +30,9 @@
 class Case < ApplicationRecord
   include Comparable
   include Mobility
+  extend FriendlyId
+
+  friendly_id :kicker, use: %i[history mobility]
 
   translates :kicker, :title, :dek, :summary, :narrative, :learning_objectives,
              :audience, :classroom_timeline, :acknowledgements, fallbacks: true
