cbca72ca4c74b5e967694f20dbe1fdec2812bc6b
Cameron Bothner <cameronbothner@gmail.com>
2018-02-20 12:32:43 -0500

    feat: allow users to edit cases they have editorships on

--- app/policies/case_policy.rb
+++ app/policies/case_policy.rb
@@ -2,13 +2,32 @@
 
 # @see Case
 class CasePolicy < ApplicationPolicy
+  # What cases can this user administrate?
+  class AdminScope < Scope
+    def resolve
+      if editor?
+        scope.all
+      else
+        scope.merge(user.my_cases)
+      end
+    end
+  end
+
   def show?
     record.published? ||
       user.enrollment_for_case(record).present? ||
       editor?
   end
 
+  def update?
+    admin_scope.where(id: record.id).exists?
+  end
+
   def destroy?
     editor? unless record.published?
+  end
+
+  def admin_scope
+    AdminScope.new(user, record.class).resolve
   end
 end
