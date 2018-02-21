480b6e2ae2a02c416f242b5345866663c03b3afc
Cameron Bothner <cameronbothner@gmail.com>
2018-02-21 14:04:22 -0500

    fix: use `inverse_of` to be able to build the join association
    It was creating the case but not associating it with the reader even
    though we were building it on the reader’s `my_cases` association. This
    is because we hadn’t listed the inverse associations on Editorship.
    
    From the docs:
    
    > If you are going to modify the association (rather than just read from
    > it), then it is a good idea to set the :inverse_of option on the
    > source association on the join model. This allows associated records
    > to be built which will automatically create the appropriate join model
    > records when they are saved. (See the 'Association Join Models'
    > section above.)

--- app/models/editorship.rb
+++ app/models/editorship.rb
@@ -4,6 +4,6 @@
 # ability to edit it. That Reader association is called {editor} to distinguish
 # it from the readers connected to a case through {Enrollment}s
 class Editorship < ApplicationRecord
-  belongs_to :case
-  belongs_to :editor, class_name: 'Reader'
+  belongs_to :case, inverse_of: :editorships
+  belongs_to :editor, class_name: 'Reader', inverse_of: :editorships
 end
