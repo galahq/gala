a4eb56951f1c321b9f1f08b982e74aebc1ad78dd
Cameron Bothner <cameronbothner@gmail.com>
2018-03-02 15:37:32 -0500

    feat: implement featured_controller #create and #destroy

--- app/controllers/cases/features_controller.rb
+++ app/controllers/cases/features_controller.rb
@@ -3,6 +3,8 @@
 module Cases
   # Featured cases appear prominently in the catalog.
   class FeaturesController < ApplicationController
+    before_action :set_case, only: %i[create destroy]
+
     # @route [GET] `/cases/features`
     def index
       enrolled = current_user.enrollments.pluck(:case_id)
@@ -11,6 +13,25 @@ module Cases
                      .limit(6)
                      .map(&:slug)
       render json: { features: features }
+    end
+
+    # @route [POST] `/cases/features`
+    def create
+      @case.update featured: true
+      redirect_to edit_case_settings_path @case
+    end
+
+    # @route [DELETE] `/cases/features/case-slug`
+    def destroy
+      @case.update featured: false
+      redirect_to edit_case_settings_path @case
+    end
+
+    private
+
+    def set_case
+      return head :bad_request if params[:case_slug].blank?
+      @case = Case.friendly.find params[:case_slug]
     end
   end
 end
