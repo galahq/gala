e91e4830082427925596f14e6fa0c67866440073
Cameron Bothner <cameronbothner@gmail.com>
2018-03-02 16:25:21 -0500

    feat: only allow Gala editors to feature a case

--- app/controllers/cases/features_controller.rb
+++ app/controllers/cases/features_controller.rb
@@ -17,12 +17,14 @@ module Cases
 
     # @route [POST] `/cases/features`
     def create
+      authorize :'cases/features'
       @case.update featured: true
       redirect_to edit_case_settings_path @case
     end
 
     # @route [DELETE] `/cases/features/case-slug`
     def destroy
+      authorize :'cases/features'
       @case.update featured: false
       redirect_to edit_case_settings_path @case
     end
