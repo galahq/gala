6f3cdf96a341dfb93ed8b78350e3865342bd126e
Cameron Bothner <cameronbothner@gmail.com>
2018-04-06 10:43:11 -0400

    style: reformats for rubocop

--- app/models/link_expansion/visibility.rb
+++ app/models/link_expansion/visibility.rb
@@ -5,7 +5,8 @@ class LinkExpansion
   # database
   #
   # @attr no_description [Boolean] Don’t use the description from OpenGraph
-  # @attr no_embed [Boolean] Show OpenGraph preview even when OEmbed is available
+  # @attr no_embed [Boolean] Show OpenGraph preview even when OEmbed is
+  #   available
   # @attr no_image [Boolean] Don’t use the image from OpenGraph
   class Visibility < ApplicationRecord
     self.table_name = :link_expansion_visibilities
