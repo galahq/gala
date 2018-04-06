a6e038fb0d65204f18e0b5888a3003bcdcfb159a
Cameron Bothner <cameronbothner@gmail.com>
2018-04-06 12:02:04 -0400

    docs: remove old incorrect stuff from edgenote docs

--- app/models/edgenote.rb
+++ app/models/edgenote.rb
@@ -5,34 +5,7 @@
 # association to {Case} for efficient preloading.
 #
 # `v2` Edgenotes have a clean design which looks less like a textbook than `v1`,
-# which is deprecated. As of `v2`, Edgenote attributes have the following
-# styles, in order of precedence.
-#
-# ### Video
-# If {youtube_slug} is set, then these attributes are used:
-# - {youtube_slug}
-# - {caption}
-#
-# ### Quotation
-# If {pull_quote} or {audio_url} are set, then these attributes are used:
-# - {pull_quote}
-# - {attribution}
-# - {audio_url} (quotation is expected to be an excerpt from the audio, if set)
-# - {caption}
-# - {call_to_action}
-# - {website_url}
-#
-# ### Image
-# If {image_url} is set, then these attributes are used:
-# - {image_url} (the image will zoom unless the Edgenote is a link)
-# - {photo_credit}
-# - {alt_text}
-# - {caption}
-# - {call_to_action}
-# - {website_url}
-#
-# @note The Edgenote will only act as a link if {website_url} and
-#   {call_to_action} are set
+# which is deprecated.
 #
 # @attr slug [String] a unique, URL-safe identifier
 # @attr style [:v1, :v2] deprecated `v1` or new-style `v2`
