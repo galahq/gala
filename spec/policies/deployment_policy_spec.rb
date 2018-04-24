4c11507bca886d3b7ea62476e5e153a9a7029fa0
Cameron Bothner <cameronbothner@gmail.com>
2018-04-24 13:46:36 -0400

    feat: allow group administrators to edit deployments of those cases

--- spec/policies/deployment_policy_spec.rb
+++ spec/policies/deployment_policy_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe DeploymentPolicy do
-  let(:reader) { DeploymentPolicy::UserContext.new build :reader }
+  let(:reader_context) { DeploymentPolicy::UserContext.new build :reader }
   let(:editor) { DeploymentPolicy::UserContext.new build :reader, :editor }
 
   let(:deployment) { build :deployment }
@@ -12,26 +12,28 @@ RSpec.describe DeploymentPolicy do
 
   permissions :update? do
     it 'does not allow an arbitrary reader to edit a deployment' do
-      expect(subject).not_to permit reader, deployment
+      expect(subject).not_to permit reader_context, deployment
     end
 
     it 'does not allow a student enrolled in the case to edit its deployment' do
-      reader.enrollments << build(:enrollment, case: deployment.case)
-      expect(subject).not_to permit reader, deployment
+      create :group_membership, group: deployment.group,
+                                reader: reader_context.reader
+      expect(subject).not_to permit reader_context, deployment
     end
 
     it 'allows an instructor to edit her own deployment' do
-      reader.enrollments << build(:enrollment, case: deployment.case,
-                                               status: :instructor)
-      expect(subject).to permit reader, deployment
+      create :group_membership, group: deployment.group,
+                                reader: reader_context.reader,
+                                status: :admin
+      expect(subject).to permit reader_context, deployment
     end
 
     it 'allows an unknown user with a valid LTI context to edit deployments ' \
        'that belong to that context' do
       deployment.group.context_id = 'context'
-      reader.selection_params = { 'context_id' => 'context' }
+      reader_context.selection_params = { 'context_id' => 'context' }
 
-      expect(subject).to permit reader, deployment
+      expect(subject).to permit reader_context, deployment
     end
 
     it 'allows an editor to edit any deployment' do
