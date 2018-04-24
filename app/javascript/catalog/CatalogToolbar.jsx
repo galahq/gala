7fd884aaf735415265ab2b052511e0f8e3b73ed6
Cameron Bothner <cameronbothner@gmail.com>
2018-04-24 17:10:50 -0400

    feat: add a button to the catalog toolbar for deployments#index

--- app/javascript/catalog/CatalogToolbar.jsx
+++ app/javascript/catalog/CatalogToolbar.jsx
@@ -31,6 +31,11 @@ const CatalogToolbar = ({ history }: ContextRouter) => (
           onClick: () =>
             (window.location = 'http://www.teachmsc.org/action/make'),
         },
+        {
+          message: 'deployments.index.teachWithCases',
+          iconName: 'follower',
+          onClick: () => (window.location = '/deployments'),
+        },
       ],
       [],
       [{ component: <Search /> }],
