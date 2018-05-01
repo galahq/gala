c8a6cab4836b2717efe656ac79f2120053fe63bb
Cameron Bothner <cameronbothner@gmail.com>
2018-05-01 10:46:44 -0400

    fix: updates language

--- app/javascript/catalog/CatalogToolbar.jsx
+++ app/javascript/catalog/CatalogToolbar.jsx
@@ -32,7 +32,7 @@ const CatalogToolbar = ({ history }: ContextRouter) => (
             (window.location = 'http://www.teachmsc.org/action/make'),
         },
         {
-          message: 'deployments.index.teachWithCases',
+          message: 'deployments.index.deployACase',
           iconName: 'follower',
           onClick: () => (window.location = '/deployments'),
         },
