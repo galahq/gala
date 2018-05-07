4d91f20155b7ab40ac8cf13c17c3f3b55e5bad80
Cameron Bothner <cameronbothner@gmail.com>
2018-05-07 11:20:02 -0400

    refactor: add a convenience method ActiveModel::Serializer.for

--- config/initializers/active_model_serializers.rb
+++ config/initializers/active_model_serializers.rb
@@ -5,3 +5,9 @@ ActiveModelSerializers.config.key_transform = :camel_lower
 ActiveSupport::Notifications.unsubscribe(
   ActiveModelSerializers::Logging::RENDER_EVENT
 )
+
+ActiveModel::Serializer.class_eval do
+  def self.for(resource, options = {})
+    ActiveModelSerializers::SerializableResource.new(resource, options)
+  end
+end
