62aac2ffde3a9c1b40d2dfb9073ef9f6432e8718
Cameron Bothner <cameronbothner@gmail.com>
2018-05-10 15:11:44 -0400

    fix: output es5-compatible uglified strings

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -7,4 +7,8 @@ const fixMapbox = { module: { noParse: /(mapbox-gl)\.js$/ }}
 // Our code is open source anyway and I hate debugging otherwise
 const sourceMap = { devtool: 'source-map' }
 
+// Because otherwise it will transform "📷" to "\u{1f4f7}", which is a syntax
+// error on... oh, you know, googlebot
+environment.plugins.get('UglifyJs').options.uglifyOptions.output.ecma = 5
+
 module.exports = merge(environment.toWebpackConfig(), fixMapbox, sourceMap)
