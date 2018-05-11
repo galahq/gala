7e83a0d4df38ecbff47fa247bbc78298fbcb5f15
Cameron Bothner <cameronbothner@gmail.com>
2018-05-11 12:03:11 -0400

    fix: add pt-dark to header of activity page

--- app/javascript/elements/Activity.jsx
+++ app/javascript/elements/Activity.jsx
@@ -39,7 +39,7 @@ const Activity = ({
 }) => (
   <article>
     <section className="Page-meta">
-      <h1>
+      <h1 className="pt-dark">
         <EditableText
           multiline
           placeholder="Activity title"
