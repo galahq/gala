a564b410f7df65fd4d3e2fba3b14f21869c7df8f
Cameron Bothner <cameronbothner@gmail.com>
2018-05-17 13:35:29 -0400

    fix: add placeholder to cards

--- app/javascript/card/CardContents.jsx
+++ app/javascript/card/CardContents.jsx
@@ -6,6 +6,7 @@
 import * as React from 'react'
 import styled, { css } from 'styled-components'
 import { append } from 'ramda'
+import { injectIntl } from 'react-intl'
 
 import { Editor, EditorState } from 'draft-js'
 import { Route } from 'react-router-dom'
@@ -22,13 +23,14 @@ import { OnScreenTracker } from 'utility/Tracker'
 import { FocusContainer } from 'utility/A11y'
 import { ScrollIntoView } from 'utility/ScrollView'
 
+import type { IntlShape } from 'react-intl'
 import type { CardProps } from 'card'
 
 const CommentThreadsCard = asyncComponent(() =>
   import('comments/CommentThreadsCard').then(m => m.default)
 )
 
-class CardContents extends React.Component<CardProps, *> {
+class CardContents extends React.Component<CardProps & { intl: IntlShape }, *> {
   // We have to be able to respond to props change that would change
   // customStyleMap by "jiggling" each block of editorState to trigger a
   // rerender. This internal state should exactly track props, plus jiggle.
@@ -92,6 +94,7 @@ class CardContents extends React.Component<CardProps, *> {
       deletable,
       title,
       match,
+      intl,
     } = this.props
     const { editorState } = this.state
 
@@ -130,6 +133,9 @@ class CardContents extends React.Component<CardProps, *> {
           priority={100}
         >
           <Editor
+            placeholder={intl.formatMessage({
+              id: 'cards.edit.writeSomething',
+            })}
             readOnly={readOnly}
             customStyleMap={styleMap}
             editorState={editorState}
@@ -178,7 +184,7 @@ class CardContents extends React.Component<CardProps, *> {
   }
 }
 
-export default CardContents
+export default injectIntl(CardContents)
 
 function citationInsideThisCard (card: ?Element, citation: ?Element): boolean {
   if (!card || !citation) return false
