42be2c78ca280b31f4305073fe10f83b281b9af6
Cameron Bothner <cameronbothner@gmail.com>
2018-05-18 16:00:21 -0400

    feat: create Lock model

---
+++ app/models/lock.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# A lock prevents a user from editing a model that another user is already
+# editing. A user must take a lock on a model before writing to it.
+class Lock < ApplicationRecord
+  belongs_to :lockable, polymorphic: true
+  belongs_to :reader
+  belongs_to :case
+end
