9e472d6e437da158b56fcb5de3db01c3ad9aae19
Cameron Bothner <cameronbothner@gmail.com>
2018-05-19 16:05:59 -0400

    feat: broadcast edits of Watchable types to their case’s EditsChannel

---
+++ app/jobs/edit_broadcast_job.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+# Broadcast edits to a case
+class EditBroadcastJob < ActiveJob::Base
+  queue_as :default
+
+  def perform(watchable, case_slug:, cached_params:)
+    @watchable = watchable
+    @case_slug = case_slug
+
+    broadcast_edit
+  end
+
+  rescue_from ActiveJob::DeserializationError do |_exception|
+    @watchable = @serialized_arguments[1]['cached_params']
+    @case_slug = @serialized_arguments[1]['case_slug']
+    broadcast_edit type: :destroyed
+  end
+
+  private
+
+  def broadcast_edit(type: edit_type)
+    EditsChannel.broadcast_to @case_slug,
+                              type: type, watchable: serialized_watchable
+  end
+
+  def edit_type
+    if @watchable.updated?
+      :updated
+    else
+      :created
+    end
+  end
+
+  def serialized_watchable
+    ActiveModel::Serializer.for(@watchable).as_json
+  end
+end
