330b1d921387fa101c8d7118e9a7b782e956bb70
Cameron Bothner <cameronbothner@gmail.com>
2018-05-22 13:52:45 -0400

    fix: serialize a page’s list of card ids as strings

--- app/serializers/page_serializer.rb
+++ app/serializers/page_serializer.rb
@@ -4,6 +4,6 @@
 class PageSerializer < ApplicationSerializer
   attributes :id, :position, :title
   attribute(:url) { page_path I18n.locale, object }
-  attribute(:cards) { object.cards.map(&:id) }
+  attribute(:cards) { object.cards.map(&:to_param) }
   belongs_to :case_element
 end
