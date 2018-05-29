225e17b6a352f0a8a3563919e0e6bee980860a34
Cameron Bothner <cameronbothner@gmail.com>
2018-05-29 17:12:04 -0400

    feat: add a few more oembed providers

--- config/initializers/oembed.rb
+++ config/initializers/oembed.rb
@@ -20,3 +20,16 @@ OEmbed::Providers.register juxtapose
 timeline = OEmbed::Provider.new('https://oembed.knightlab.com/timeline/')
 timeline << 'https://cdn.knightlab.com/libs/timeline*'
 OEmbed::Providers.register timeline
+
+social_ex = OEmbed::Provider.new 'https://www.socialexplorer.com/services/oembed/'
+social_ex << 'https://www.socialexplorer.com/*/explore'
+social_ex << 'https://www.socialexplorer.com/*/view'
+social_ex << 'https://www.socialexplorer.com/*/edit'
+social_ex << 'https://www.socialexplorer.com/*/embed'
+OEmbed::Providers.register social_ex
+
+sketchfab = OEmbed::Provider.new 'http://sketchfab.com/oembed'
+sketchfab << 'http://sketchfab.com/models/*'
+sketchfab << 'https://sketchfab.com/models/*'
+sketchfab << 'https://sketchfab.com/*/folders/*'
+OEmbed::Providers.register sketchfab
