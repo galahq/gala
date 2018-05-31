5d619d7bdef262a05553360db19e98bacef3ec35
Cameron Bothner <cameronbothner@gmail.com>
2018-05-31 17:13:14 -0400

    fix: make editor_email a more proper virtual attribute

--- app/models/editorship.rb
+++ app/models/editorship.rb
@@ -9,20 +9,27 @@ class Editorship < ApplicationRecord
   belongs_to :case, inverse_of: :editorships
   belongs_to :editor, class_name: 'Reader', inverse_of: :editorships
 
-  before_validation :set_editor_from_email
+  after_find :initialize_editor_email
   before_validation :set_editor_from_email, if: :editor_email_changed?
   after_create_commit :enroll_editor_in_case
 
   delegate :name, to: :editor, prefix: true, allow_nil: true
-
-  def set_editor_from_email
-    self.editor = Reader.find_by_email editor_email
-  end
 
   # Readers who have an editorship on the case should be automatically be
   # enrolled in it
   def enroll_editor_in_case
     Enrollment.find_or_create_by case_id: self.case.id,
                                  reader_id: editor.id
+  end
+
+  private
+
+  def initialize_editor_email
+    self.editor_email = editor&.email
+    clear_attribute_changes %i[editor_email]
+  end
+
+  def set_editor_from_email
+    self.editor = Reader.find_by_email editor_email
   end
 end
