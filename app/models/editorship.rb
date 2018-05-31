b5c9265b3625f3d3200e9d16079887c307a9433a
Cameron Bothner <cameronbothner@gmail.com>
2018-05-31 17:10:49 -0400

    feat: allow editors of a case to invite other users to edit with them

--- app/models/editorship.rb
+++ app/models/editorship.rb
@@ -4,12 +4,19 @@
 # ability to edit it. That Reader association is called {editor} to distinguish
 # it from the readers connected to a case through {Enrollment}s
 class Editorship < ApplicationRecord
+  attribute :editor_email, :string
+
   belongs_to :case, inverse_of: :editorships
   belongs_to :editor, class_name: 'Reader', inverse_of: :editorships
 
+  before_validation :set_editor_from_email
   after_create_commit :enroll_editor_in_case
 
-  delegate :name, to: :editor, prefix: true
+  delegate :name, to: :editor, prefix: true, allow_nil: true
+
+  def set_editor_from_email
+    self.editor = Reader.find_by_email editor_email
+  end
 
   # Readers who have an editorship on the case should be automatically be
   # enrolled in it
