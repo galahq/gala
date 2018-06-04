73eb65f5603fde2f82f012aa60dfd83a617687c8
Cameron Bothner <cameronbothner@gmail.com>
2018-06-04 14:39:46 -0400

    docs: install and configure docusaurus [ci skip]

---
+++ docs/website/siteConfig.js
@@ -0,0 +1,44 @@
+const siteConfig = {
+  title: 'How to use Gala',
+  tagline: 'Open access learning tools for sustainability science',
+  url: 'https://docs.learngala.com',
+  baseUrl: '/',
+
+  projectName: 'gala-docs',
+  organizationName: 'galahq',
+
+  headerLinks: [
+    { doc: 'authoring-getting-started', label: 'Authoring' },
+    { doc: 'teaching-getting-started', label: 'Teaching' },
+    { page: 'help', label: 'Help' },
+  ],
+
+  headerIcon: 'img/gala-logo.svg',
+  footerIcon: 'img/gala-logo.svg',
+
+  colors: {
+    primaryColor: '#02284B',
+    secondaryColor: '#1D3F5E',
+  },
+
+  fonts: {
+    sansSerif: ['Tenso', 'system-ui'],
+  },
+
+  copyright: `Copyright © ${new Date().getFullYear()} Regents of the University of Michigan`,
+
+  highlight: {
+    theme: 'default',
+  },
+
+  scripts: ['https://buttons.github.io/buttons.js'],
+
+  onPageNav: 'separate',
+
+  ogImage: 'img/gala-logo.svg',
+  twitterImage: 'img/gala-logo.svg',
+
+  repoUrl: 'https://github.com/galahq/gala',
+}
+
+module.exports = siteConfig
