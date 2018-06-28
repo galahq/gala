0c2e6945c4feffb6636b920dcec04e8e9ba294bf
Cameron Bothner <cameronbothner@gmail.com>
2018-06-28 08:59:25 -0400

    feat: serialize tags richly with cases
    We were going to maybe just use strings, but the need for tags to be
    translated requires we always also pass down the unique English name

--- app/serializers/cases/preview_serializer.rb
+++ app/serializers/cases/preview_serializer.rb
@@ -9,6 +9,8 @@ module Cases
                :published_at, :slug, :small_cover_url, :summary, :title,
                :translators, :zoom
 
+    has_many :tags
+
     link(:self) { case_path(I18n.locale, object) }
     link(:settings) { edit_case_settings_path(I18n.locale, object) }
     link(:teach) { new_deployment_path(case_slug: object.slug) }
