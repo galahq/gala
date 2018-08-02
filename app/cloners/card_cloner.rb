971278aeed8bf7f1831479c082571b10a73bbef3
Cameron Bothner <cameronbothner@gmail.com>
2018-08-02 12:07:05 -0400

    fix: don't mess up acts_as_list position while cloning
    Card clones were being saved in an intermediate state, which messed up 
    the positions of the origin card’s siblings

--- app/cloners/card_cloner.rb
+++ app/cloners/card_cloner.rb
@@ -3,9 +3,11 @@
 # @see Card
 class CardCloner < Clowne::Cloner
   finalize do |_source, record, kase:, **|
-    record.case = kase
-    record.content_state =
-      ContentStateCloner.call record.content_state, kase: kase
-    record.save!
+    Card.acts_as_list_no_update do
+      record.case = kase
+      record.content_state =
+        ContentStateCloner.call record.content_state, kase: kase
+      record.save!
+    end
   end
 end
