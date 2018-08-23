16852ceea3eed034f8b99ad5535cc631745b6bba
Cameron Bothner <cameronbothner@gmail.com>
2018-08-23 15:02:52 -0400

    refactor: extract generic attachments controller for detachment

---
+++ spec/controllers/edgenotes/attachments_controller_spec.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Edgenotes::AttachmentsController do
+  let(:kase) { create :case_with_edgenotes, library: nil }
+  let(:reader) { kase.editors.create attributes_for :reader }
+  let(:edgenote) { kase.edgenotes.first }
+
+  before do
+    data = {
+      io: StringIO.new('Hello world!'),
+      filename: 'hello.txt',
+      content_type: 'text/plain'
+    }
+    edgenote.image.attach data
+
+    sign_in reader
+  end
+
+  describe 'DELETE #destroy' do
+    it 'detaches an attached image' do
+      delete :destroy, params: { edgenote_slug: edgenote.slug,
+                                 attribute: 'image' }
+      edgenote.reload
+      expect(edgenote.image.attached?).to be_falsey
+    end
+
+    it 'authorizes user on her ability to update the edgenote’s case' do
+      reader.editorships.find_by(case: kase).destroy
+      delete :destroy, params: { edgenote_slug: edgenote.slug,
+                                 attribute: 'image' }
+      expect(response.status).to eq 302
+    end
+
+    it 'returns 404 if the edgenote can’t be found' do
+      delete :destroy, params: { edgenote_slug: 'imaginary-edgenote',
+                                 attribute: 'private_file' }
+      expect(response.status).to eq 404
+    end
+
+    it 'limits users to an allowlist of attribute names' do
+      delete :destroy, params: { edgenote_slug: edgenote.slug,
+                                 attribute: 'private_file' }
+      expect(response.status).to eq 404
+    end
+  end
+end
