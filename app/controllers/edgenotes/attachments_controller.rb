a5ba2e6afde7f1c5241f1fb4e25b985670561fc9
Cameron Bothner <cameronbothner@gmail.com>
2018-08-23 15:02:52 -0400

    refactor: extract generic attachments controller for detachment

--- app/controllers/edgenotes/attachments_controller.rb
+++ app/controllers/edgenotes/attachments_controller.rb
@@ -2,28 +2,9 @@
 
 module Edgenotes
   # Allow {Edgenote} attachments to be detached
-  class AttachmentsController < ApplicationController
-    ATTACHMENT_ATTRIBUTES = %w[image audio].freeze
-
-    # @param [DELETE] `/edgenotes/slug/attachments/attribute`
-    def destroy
-      return head :not_found unless set_edgenote && set_attachment
-      authorize @edgenote.case, :update?
-
-      @attachment.detach
-
-      head :no_content
-    end
-
-    private
-
-    def set_edgenote
-      @edgenote = Edgenote.find_by slug: params[:edgenote_slug]
-    end
-
-    def set_attachment
-      return nil unless ATTACHMENT_ATTRIBUTES.include? params[:attribute]
-      @attachment = @edgenote.send params[:attribute]
-    end
+  class AttachmentsController < ::AttachmentsController
+    model -> { Edgenote.find_by slug: params[:edgenote_slug] }
+    permit_attributes :image, :audio
+    authorize_action ->(model) { authorize model.case, :update? }
   end
 end
