92e12e60b10d11eb333968657cce27db7694fa39
Cameron Bothner <cameronbothner@gmail.com>
2018-08-23 15:52:01 -0400

    feat: add delete teaching guide button

--- app/serializers/cases/preview_serializer.rb
+++ app/serializers/cases/preview_serializer.rb
@@ -11,10 +11,6 @@ module Cases
 
     has_many :tags
 
-    link(:new_translation) { new_case_translation_path object }
     link(:self) { case_path object }
-    link(:settings) { edit_case_settings_path object }
-    link(:taggings) { case_taggings_path object }
-    link(:teach) { new_deployment_path case_slug: object.slug }
   end
 end
