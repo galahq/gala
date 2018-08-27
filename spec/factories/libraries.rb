34b57b5421bbb9818e14113700dc0b8990adc533
Cameron Bothner <cameronbothner@gmail.com>
2018-08-27 17:32:28 -0400

    test: test the library admin stuff

--- spec/factories/libraries.rb
+++ spec/factories/libraries.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :library do
-    sequence(:slug) { |n| "michigan-sustainaility-cases-#{n}"}
-    name "Michigan Sustainability Cases"
-    logo_url "https://msc-gala.imgix.net/block-m.svg"
-    background_color "#00274c"
-    foreground_color "#ffcb05"
+    sequence(:slug) { |n| "michigan-sustainaility-cases-#{n}" }
+    name 'Michigan Sustainability Cases'
+    background_color '#00274c'
+    foreground_color '#ffcb05'
   end
 end
---
+++ spec/features/creating_a_library_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+feature 'Creating a library' do
+  let(:reader) { create :reader, :editor }
+
+  before { login_as reader }
+
+  scenario 'is possible' do
+    visit libraries_path
+    expect(page).to have_content 'Create a New Library'
+
+    fill_in 'Name', with: 'Michigan Sustainability Cases'
+    fill_in 'Background Color', with: '#00274c'
+    fill_in 'Foreground Color', with: '#ffcb05'
+    attach_file File.absolute_path('./app/assets/images/gala-logo.svg'),
+                name: 'library[logo]', make_visible: true
+    click_button 'Create Library'
+
+    expect(page).to have_content 'successfully created'
+    expect(page).to have_content 'Michigan Sustainability Cases'
+  end
+end
