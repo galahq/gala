7ade93cf07cc0e3f11b506d47f8a8257d96cfea7
Cameron Bothner <cameronbothner@gmail.com>
2018-08-28 12:02:45 -0400

    fix: don’t show libraries in the catalog that the user can’t access

--- app/controllers/catalog/libraries_controller.rb
+++ app/controllers/catalog/libraries_controller.rb
@@ -5,7 +5,7 @@ module Catalog
   class LibrariesController < ApplicationController
     # @route [GET] `/catalog/libraries`
     def index
-      @libraries = Library.visible_in_catalog.ordered
+      @libraries = policy_scope(Library).visible_in_catalog.ordered
       render json: @libraries.decorate
     end
   end
