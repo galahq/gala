925ac07c5f8e739bf0fc9637fcde8c6beca410ab
Cameron Bothner <cameronbothner@gmail.com>
2018-08-28 15:01:28 -0400

    fix: reduces the amount sent about each case in cases#index

--- app/serializers/cases/preview_serializer.rb
+++ app/serializers/cases/preview_serializer.rb
@@ -3,11 +3,8 @@
 module Cases
   # A subset of case data
   class PreviewSerializer < ApplicationSerializer
-    attributes :acknowledgements, :audience, :authors, :commentable, :cover_url,
-               :dek, :featured_at, :kicker, :latitude, :learning_objectives,
-               :longitude, :photo_credit, :published_at, :slug,
-               :small_cover_url, :summary, :teaching_guide_url, :title,
-               :translators, :zoom
+    attributes :cover_url, :dek, :featured_at, :kicker, :latitude, :longitude,
+               :photo_credit, :published_at, :slug, :small_cover_url, :title
 
     has_many :tags
 
