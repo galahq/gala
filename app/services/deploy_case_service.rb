2eb319897b74c8ca0cb994f1044c2d0ba9e7f578
Cameron Bothner <cameronbothner@gmail.com>
2018-09-04 11:53:15 -0400

    feat: deploying a case registers a reader as an instructor

---
+++ app/services/deploy_case_service.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+# Deploy a case, and all that entails
+class DeployCaseService
+  attr_reader :deployment, :reader
+
+  delegate :group, to: :deployment
+
+  def initialize(deployment_params, reader)
+    @deployment = Deployment.new deployment_params
+    @reader = reader
+  end
+
+  def call
+    ActiveRecord::Base.transaction do
+      save_deployment
+      add_reader_as_group_administrator
+      invite_reader_to_caselog
+      enroll_reader_as_instructor
+    rescue ActiveRecord::RecordInvalid, ActiveRecord::RecordNotSaved
+      return nil
+    end
+    deployment
+  end
+
+  private
+
+  def save_deployment
+    deployment.save!
+  end
+
+  def add_reader_as_group_administrator
+    group.add_administrator reader
+  end
+
+  def enroll_reader_as_instructor
+    Enrollment.upsert enrollment_params
+  end
+
+  def enrollment_params
+    { case_id: deployment.case_id, reader_id: reader.id,
+      active_group_id: group.id, status: :instructor }
+  end
+
+  def invite_reader_to_caselog
+    reader.add_role :instructor
+  end
+end
