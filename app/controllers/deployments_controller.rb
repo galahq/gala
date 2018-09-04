9591125eaa1d2d13f5933b32a3ad60ad175ea500
Cameron Bothner <cameronbothner@gmail.com>
2018-09-04 11:53:15 -0400

    feat: deploying a case registers a reader as an instructor

--- app/controllers/deployments_controller.rb
+++ app/controllers/deployments_controller.rb
@@ -21,14 +21,13 @@ class DeploymentsController < ApplicationController
   end
 
   def create
-    @deployment = Deployment.new deployment_params
-    prepare_for_form
+    service = DeployCaseService.new deployment_params, current_reader
 
-    if @deployment.save
-      @deployment.group.add_administrator current_reader
+    if (@deployment = service.call)
       redirect_to helpers.focus_deployment_path(@deployment),
                   notice: successfully_created
     else
+      prepare_for_form
       render :new
     end
   end
