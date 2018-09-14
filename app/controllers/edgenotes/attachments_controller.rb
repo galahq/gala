e8ae5f6f2f9276a03dba8ae8e27bf8f69b331ed6
Cameron Bothner <cameronbothner@gmail.com>
2018-09-14 13:10:13 -0400

    fix: permit file attachments to be deleted

--- app/controllers/edgenotes/attachments_controller.rb
+++ app/controllers/edgenotes/attachments_controller.rb
@@ -4,7 +4,7 @@ module Edgenotes
   # Allow {Edgenote} attachments to be detached
   class AttachmentsController < ::AttachmentsController
     model -> { Edgenote.find_by slug: params[:edgenote_slug] }
-    permit_attributes :image, :audio
+    permit_attributes :image, :audio, :file
     authorize_action ->(model) { authorize model.case, :update? }
   end
 end
