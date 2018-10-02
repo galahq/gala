1e7c1491a58167cd51b6579cbc4b06c82d2e5296
Cameron Bothner <cameronbothner@gmail.com>
2018-10-02 15:50:22 -0400

    feat: accept and serialize urls for comment attachments

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -3,7 +3,12 @@
 # @see Comment
 class CommentSerializer < ApplicationSerializer
   attributes :id, :content, :timestamp, :comment_thread_id
+  attribute :attachment_urls
   attribute :reader
+
+  def attachment_urls
+    object.attachments.map { |a| polymorphic_path a, only_path: true }
+  end
 
   def reader
     object.reader.slice :name, :initials, :id, :image_url, :hash_key
