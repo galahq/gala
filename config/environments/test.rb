bd2d506faa3dd31b9eb7b2b4f19f2130e2b010dd
Cameron Bothner <cameronbothner@gmail.com>
2018-10-03 17:15:26 -0400

    test: run jobs inline in tests

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -42,6 +42,8 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  config.active_job.queue_adapter = :inline
+
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
