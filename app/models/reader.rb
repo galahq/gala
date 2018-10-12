f10aca8f56d0b9cebdae622fde02ac2c780218e3
Cameron Bothner <cameronbothner@gmail.com>
2018-10-12 12:14:44 -0400

    fix: improve error message for large attachments

--- app/models/reader.rb
+++ app/models/reader.rb
@@ -54,7 +54,7 @@ class Reader < ApplicationRecord
   before_update :set_created_password, if: :encrypted_password_changed?
 
   validates :image, size: { less_than: 2.megabytes,
-                            message: 'is bigger than 2MB' },
+                            message: 'cannot be larger than 2 MB' },
                     content_type: { in: %w[image/png image/jpeg],
                                     message: 'must be JPEG or PNG' }
 
