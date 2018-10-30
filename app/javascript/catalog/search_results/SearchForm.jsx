1a1f911a050dee73a49b8ad02fdfd008cd00b425
Cameron Bothner <cameronbothner@gmail.com>
2018-10-30 14:52:20 -0400

    refactor: use hooks for search form

--- app/javascript/catalog/search_results/SearchForm.jsx
+++ app/javascript/catalog/search_results/SearchForm.jsx
@@ -17,83 +17,63 @@ import KeywordsChooser from 'overview/keywords/KeywordsChooser'
 
 import type { IntlShape } from 'react-intl'
 import type { ContextRouter } from 'react-router-dom'
-import type { Query } from 'catalog/search_results'
+import type { Query } from 'catalog/search_results/getQueryParams'
 import type { Tag } from 'redux/state'
 
 type Props = {| ...ContextRouter, params: Query, intl: IntlShape |}
-type State = { q: string, libraries: string[], tags: Tag[] }
-class SearchForm extends React.Component<Props, State> {
-  state = {
-    q: (this.props.params.q || []).join(' '),
-    libraries: this.props.params.libraries,
-    tags: makeDummyTags(this.props.params.tags),
-  }
+function SearchForm ({ history, intl, params }: Props) {
+  const queryFromUrl = (params.q || []).join(' ')
+  const [query, setQuery] = React.useState(queryFromUrl)
+  React.useEffect(() => setQuery(queryFromUrl), [params.query])
 
-  get stateForSearchParams () {
-    const { q, libraries, tags } = this.state
-    return { q, libraries, tags: tags.map(tag => tag.name) }
-  }
+  const [tagObjects, setTagObjects] = React.useState(
+    createTagObjects(params.tags)
+  )
 
-  componentDidUpdate (prevProps: Props) {
-    if (
-      this.props.params.q === prevProps.params.q &&
-      this.props.params.libraries === prevProps.params.libraries
-    ) {
-      return
-    }
-
-    this.setState({
-      q: (this.props.params.q || []).join(' '),
-      libraries: this.props.params.libraries,
-    })
-  }
-
-  handleChangeQuery = (e: SyntheticInputEvent<HTMLInputElement>) =>
-    this.setState({ q: e.currentTarget.value })
-
-  handleChangeTags = (tags: Tag[]) => this.setState({ tags })
-
-  handleSubmit = (e: SyntheticEvent<HTMLFormElement>) => {
+  function handleSubmit (e: SyntheticEvent<HTMLFormElement>) {
     e.preventDefault()
-    this.props.history.push(getSearchPath(this.stateForSearchParams))
+    const searchPath = getSearchPath({
+      q: query,
+      libraries: params.libraries,
+      tags: tagObjects.map(tag => tag.name),
+    })
+    history.push(searchPath)
   }
 
-  render () {
-    const { q, tags } = this.state
-    return (
-      <CatalogSection className="pt-dark">
-        <SectionTitle>
-          <FormattedMessage id="search.refine" />
-        </SectionTitle>
+  return (
+    <CatalogSection className="pt-dark">
+      <SectionTitle>
+        <FormattedMessage id="search.refine" />
+      </SectionTitle>
 
-        <form onSubmit={this.handleSubmit}>
-          <FormGroup>
-            <InputGroup
-              className="pt-fill"
-              leftIcon="search"
-              placeholder={this.props.intl.formatMessage({
-                id: 'search.fullTextSearch',
-              })}
-              value={q}
-              onChange={this.handleChangeQuery}
-            />
-          </FormGroup>
+      <form onSubmit={handleSubmit}>
+        <FormGroup>
+          <InputGroup
+            className="pt-fill"
+            leftIcon="search"
+            placeholder={intl.formatMessage({
+              id: 'search.fullTextSearch',
+            })}
+            value={query}
+            onChange={e => setQuery(e.target.value)}
+          />
+        </FormGroup>
 
-          <FormGroup label={<FormattedMessage id="catalog.keywords" />}>
-            <KeywordsChooser tags={tags} onChange={this.handleChangeTags} />
-          </FormGroup>
+        <FormGroup label={<FormattedMessage id="catalog.keywords" />}>
+          <KeywordsChooser tags={tagObjects} onChange={setTagObjects} />
+        </FormGroup>
 
-          <SubmitButton>
-            <FormattedMessage id="search.search" />
-          </SubmitButton>
-        </form>
-      </CatalogSection>
-    )
-  }
+        <SubmitButton>
+          <FormattedMessage id="search.search" />
+        </SubmitButton>
+      </form>
+    </CatalogSection>
+  )
 }
+
 export default injectIntl(withRouter(SearchForm))
 
-function makeDummyTags (names: ?(string[])) {
+function createTagObjects (names: ?(string[])): Tag[] {
   return names ? names.map(name => ({ name, displayName: name })) : []
 }
 
