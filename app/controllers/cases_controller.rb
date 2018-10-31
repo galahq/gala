13f866d4db71ca16d7fdda658a1d848b099ab7b9
Cameron Bothner <cameronbothner@gmail.com>
2018-10-31 11:16:33 -0400

    fix: allow cases to be deleted even when they have comments

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -92,7 +92,7 @@ class CasesController < ApplicationController
   # @route [DELETE] `/cases/slug`
   def destroy
     authorize @case
-    @case.destroy
+    @case.destroy!
     redirect_to my_cases_path, notice: successfully_destroyed
   end
 
