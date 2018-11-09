129b4aa178f5b1a15af4a5deba4ff0b1d650d0e2
Cameron Bothner <cameronbothner@gmail.com>
2018-11-09 12:01:53 -0500

    refactor: extract QuizUpdater from CustomizeDeploymentService
    Co-Authored-By: Pearl Zeng <pearlzhuzeng@users.noreply.github.com>

---
+++ app/services/quiz_updater.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+class QuizUpdater
+  attr_reader :quiz
+
+  def initialize(quiz)
+    @quiz = quiz
+  end
+
+  def update(quiz_params)
+    quiz_params = quiz_params.to_h.transform_keys(&:to_s)
+    questions = quiz_params.delete 'questions'
+
+    quiz.update quiz_params
+
+    return if questions.nil?
+    delete_questions_not_included_in questions
+    upsert_questions questions
+  end
+
+  private
+
+  def upsert_questions(questions)
+    questions.each do |question|
+      upsert_question question
+    end
+  end
+
+  def upsert_question(question)
+    if question['id'].present?
+      Question.find(question['id']).update! question
+    else
+      quiz.custom_questions.create! question
+    end
+  end
+
+  def delete_questions_not_included_in(questions)
+    question_ids = questions.map { |q| q['id'] }
+    quiz.custom_questions.each do |old_question|
+      old_question.destroy unless question_ids.include? old_question.id
+    end
+  end
+end
