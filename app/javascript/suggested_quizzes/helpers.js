7626c3909f2dc57a6b0532dad2ba2d49629ab781
Cameron Bothner <cameronbothner@gmail.com>
2018-11-13 16:53:55 -0500

    fix: error when there are blank questions
    Co-Authored-By: Pearl Zeng <pearlzhuzeng@users.noreply.github.com>

--- app/javascript/suggested_quizzes/helpers.js
+++ app/javascript/suggested_quizzes/helpers.js
@@ -6,13 +6,19 @@ import { map } from 'ramda'
 
 import type { DraftQuestion } from 'redux/state'
 
-const questionHasError = (question: DraftQuestion) =>
-  !question.content ||
-  (question.options.length === 0
-    ? question.correctAnswer === '' || question.correctAnswer == null
-    : !question.options.some(
-      (option: string) => option === question.correctAnswer
-    ))
+export const questionHasError = ({
+  content,
+  options,
+  correctAnswer,
+}: DraftQuestion) => {
+  if (!content) return true
+
+  if (options.length === 0) return correctAnswer === '' || correctAnswer == null
+
+  if (options.some(o => o === '')) return true
+
+  return !options.some(o => o === correctAnswer)
+}
 
 export const validatedQuestions: (DraftQuestion[]) => DraftQuestion[] = map(
   (question: DraftQuestion) => ({
