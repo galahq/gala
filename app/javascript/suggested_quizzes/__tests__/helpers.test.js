bc67b10dc4895558f7ba33b882d5842280d5fde0
Cameron Bothner <cameronbothner@gmail.com>
2018-11-13 16:53:55 -0500

    fix: error when there are blank questions
    Co-Authored-By: Pearl Zeng <pearlzhuzeng@users.noreply.github.com>

---
+++ app/javascript/suggested_quizzes/__tests__/helpers.test.js
@@ -0,0 +1,35 @@
+/**
+ * @flow
+ */
+
+import { questionHasError, validatedQuestions } from '../helpers'
+
+describe('questionHasError', () => {
+  test.each`
+    question                                                                      | hasError
+    ${{ id: null, content: '', options: [], correctAnswer: '' }}                  | ${true}
+    ${{ id: null, content: '', options: ['A', 'B'], correctAnswer: 'A' }}         | ${true}
+    ${{ id: null, content: 'What is?', options: [], correctAnswer: '' }}          | ${true}
+    ${{ id: null, content: 'What is?', options: [], correctAnswer: 'IDK' }}       | ${false}
+    ${{ id: null, content: 'What is?', options: ['A', 'B'], correctAnswer: '' }}  | ${true}
+    ${{ id: null, content: 'What is?', options: ['A', 'B'], correctAnswer: 'C' }} | ${true}
+    ${{ id: null, content: 'What is?', options: ['A', 'B'], correctAnswer: 'A' }} | ${false}
+    ${{ id: null, content: 'What is?', options: [''], correctAnswer: '' }}        | ${true}
+  `(
+  'returns $hasError when question is { content: $question.content, options: $question.options, correctAnswer: $question.correctAnswer }',
+  ({ question, hasError }) => {
+    expect(questionHasError(question)).toEqual(hasError)
+  }
+)
+})
+
+describe('validatedQuestions', () => {
+  test('works', () => {
+    const validated = validatedQuestions([
+      { id: null, content: 'What?', options: ['A', 'B'], correctAnswer: 'A' },
+      { id: null, content: '', options: [], correctAnswer: '' },
+    ])
+
+    expect(validated.map(q => q.hasError)).toEqual([false, true])
+  })
+})
