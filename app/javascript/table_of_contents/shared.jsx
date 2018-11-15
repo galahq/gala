68be128976e34c2ec07bc969a552e985b403e6fa
Cameron Bothner <cameronbothner@gmail.com>
2018-11-15 14:57:36 -0500

    refactor: use react-beautiful-dnd for table of contents

---
+++ app/javascript/table_of_contents/shared.jsx
@@ -0,0 +1,111 @@
+/*
+ * @flow
+ */
+
+import styled, { css } from 'styled-components'
+import { NavLink } from 'react-router-dom'
+
+export const Title = styled.h2`
+  color: #faf9f5;
+  font-family: ${p => p.theme.sansFont};
+  font-size: 13px;
+  text-transform: uppercase;
+  letter-spacing: 0.7px;
+  margin: 1.6em 0 0.8em 0;
+`
+
+export const OuterContainer = styled.div.attrs({ className: 'pt-dark' })``
+
+export const Container = styled.nav.attrs(p => ({
+  className: `c-toc ${p.disabled ? 'c-toc--disabled' : ''}`,
+}))``
+
+export const NoElements = styled.p`
+  margin: 0.5em;
+  opacity: 0.5;
+`
+
+export const List = styled.ol`
+  list-style: none;
+  font-size: 15px;
+  line-height: 1.2;
+  margin: 0;
+  margin-bottom: 1em;
+  padding: 0;
+  width: 100%;
+
+  ${p =>
+    p.isDraggingOver &&
+    css`
+      & ${Link}:hover {
+        background-color: transparent !important;
+      }
+    `}
+`
+
+export const Item = styled.li``
+
+export const Link = styled(NavLink)`
+  color: #ebeae4;
+  display: flex;
+  flex-direction: row;
+
+  ${p =>
+    p.isDragging
+      ? css`
+          background-color: #29394a !important;
+          color: #ebeae4 !important;
+        `
+      : css`
+          &:hover,
+          &[aria-current] {
+            background-color: #29394a;
+            color: #ebeae4 !important;
+          }
+        `}
+`
+
+export const Label = styled.div`
+  align-items: center;
+  display: flex;
+  flex: 0 0 45px;
+  font-weight: 800;
+  justify-content: center;
+  transform: scale(1.1);
+`
+
+export const Details = styled.div`
+  align-items: center;
+  border-top: 1px solid rgba(235, 234, 228, 0.2);
+  display: flex;
+  flex: 1;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 11px 0;
+
+  ${Item}:first-of-type & {
+    border-top: none;
+  }
+`
+
+export const ElementIcon = styled(Label)`
+  padding-right: 0.5em;
+`
+
+export const Actions = styled.div.attrs({
+  className: props =>
+    `c-toc__actions pt-button-group pt-fill ${
+      props.vertical ? 'pt-vertical' : ''
+    }`,
+})``
+
+export const AddButton = styled.button.attrs({
+  type: 'button',
+  className: 'pt-button pt-icon-add',
+})``
+
+export const AssessmentButton = styled(Link).attrs({
+  className: 'pt-button pt-fill pt-icon-properties pt-intent-success',
+})`
+  margin-top: 1em;
+`
