a3769ddab28fc4bf1c90e46905ad8a79c6f67c0b
Cameron Bothner <cameronbothner@gmail.com>
2018-12-21 11:25:54 -0500

    test: improve tests for react-intl

--- app/javascript/shared/__tests__/react-intl.test.js
+++ app/javascript/shared/__tests__/react-intl.test.js
@@ -1,58 +1,70 @@
+/**
+ * @flow
+ */
+
 import { FormattedList } from '../react-intl'
 
 import React from 'react'
-import renderer from 'react-test-renderer'
+import { render } from 'react-testing-library'
 
-jest.mock('react-intl', () => ({
-  FormattedMessage: jest.fn(props => (
-    <div data-component="FormattedMessage" {...props} />
-  )),
-}))
+import { IntlProvider } from 'react-intl'
+import loadMessages from '../../../../config/locales'
+
+async function renderWithTranslations (node, options) {
+  let messages = await loadMessages('en')
+  return render(
+    <IntlProvider locale="en" messages={messages}>
+      {node}
+    </IntlProvider>
+  )
+}
 
 describe('FormattedList', () => {
-  it('works with zero items', () => {
-    const component = renderer.create(<FormattedList list={[]} />)
-    const tree = component.toJSON()
-    expect(tree).toMatchSnapshot()
+  it('works with zero items', async () => {
+    const { container } = await renderWithTranslations(
+      <FormattedList list={[]} />
+    )
+    expect(container).toHaveTextContent(/^$/)
   })
 
-  it('works with one item', () => {
-    const component = renderer.create(<FormattedList list={['1']} />)
-    const tree = component.toJSON()
-    expect(tree).toMatchSnapshot()
+  it('works with one item', async () => {
+    const { container } = await renderWithTranslations(
+      <FormattedList list={['1']} />
+    )
+    expect(container).toHaveTextContent(/^1$/)
   })
 
-  it('works with two items', () => {
-    const component = renderer.create(<FormattedList list={['1', '2']} />)
-    const tree = component.toJSON()
-    expect(tree).toMatchSnapshot()
+  it('works with two items', async () => {
+    const { container } = await renderWithTranslations(
+      <FormattedList list={['1', '2']} />
+    )
+    expect(container).toHaveTextContent(/^1 and 2$/)
   })
 
-  it('works with three items', () => {
-    const component = renderer.create(<FormattedList list={['1', '2', '3']} />)
-    const tree = component.toJSON()
-    expect(tree).toMatchSnapshot()
+  it('works with three items', async () => {
+    const { container } = await renderWithTranslations(
+      <FormattedList list={['1', '2', '3']} />
+    )
+    expect(container).toHaveTextContent(/^1, 2, and 3$/)
   })
 
-  it('can truncate when the list is too long', () => {
-    const component = renderer.create(
+  it('can truncate when the list is too long', async () => {
+    const { container } = await renderWithTranslations(
       <FormattedList
         list={['1', '2', '3', '4', '5', '6']}
         truncate={{ after: 2, with: 'etc.' }}
       />
     )
-    const tree = component.toJSON()
-    expect(tree).toMatchSnapshot()
+    expect(container).toHaveTextContent(/^1, 2, etc.$/)
   })
 
-  it('doesn’t truncate if the list is short enough', () => {
-    const component = renderer.create(
+  it('doesn’t truncate if the list is short enough', async () => {
+    const { container } = await renderWithTranslations(
       <FormattedList
         list={['1', '2', '3', '4']}
         truncate={{ after: 5, with: 'etc.' }}
       />
     )
-    const tree = component.toJSON()
-    expect(tree).toMatchSnapshot()
+    expect(container).toHaveTextContent(/^1, 2, 3, and 4$/)
   })
 })
