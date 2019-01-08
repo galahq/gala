ccb90638495d108875ccac621e1696b542ec3aff
Cameron Bothner <cameronbothner@gmail.com>
2019-01-08 10:44:51 -0500

    refactor: move hooks into their own files

---
+++ app/javascript/utility/hooks/index.js
@@ -0,0 +1,17 @@
+/**
+ * @flow
+ */
+
+import useArray from './useArray'
+import useControllableFocus from './useControllableFocus'
+import useDocumentTitle from './useDocumentTitle'
+import useRefStack from './useRefStack'
+import useToggle from './useToggle'
+
+export {
+  useArray,
+  useControllableFocus,
+  useDocumentTitle,
+  useRefStack,
+  useToggle,
+}
