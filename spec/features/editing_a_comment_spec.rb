b1d7f80fbcd8bc58b663f622e888f7b5e6792a1c
Cameron Bothner <cameronbothner@gmail.com>
2019-01-23 16:06:32 -0500

    test: add a test for editing comments that are the first in their thread
    Co-Authored-By: Pearl Zeng <pearlzhuzeng@users.noreply.github.com>

--- spec/features/editing_a_comment_spec.rb
+++ spec/features/editing_a_comment_spec.rb
@@ -3,6 +3,45 @@
 require 'rails_helper'
 
 feature 'Editing a comment' do
+  context 'that is first in its thread' do
+    scenario 'is possible' do
+      reader = create :reader
+      kase = create :case
+      create :enrollment, reader: reader, case: kase
+      forum = kase.forums.first
+      thread = create :comment_thread, forum: forum, reader: reader
+      comment = create :comment, reader: reader, comment_thread: thread
+
+      login_as reader
+      visit comment_thread_path thread
+      expect(page).to have_content comment.content
+
+      other_reader = create :reader
+      create :enrollment, reader: other_reader, case: kase
+      Capybara.using_session :other do
+        login_as other_reader
+        visit comment_thread_path thread
+        expect(page).to have_content comment.content
+      end
+
+      click_button 'Edit comment'
+      expect(page).to have_field 'Comment text', with: comment.content
+
+      fill_in 'Comment text', with: 'Hello World!'
+      click_button 'Save'
+
+      expect(page).to have_no_content comment.content
+      expect(page).to have_content 'Hello World!'
+      expect(page).to have_content 'Edited'
+
+      Capybara.using_session :other do
+        expect(page).to have_no_content comment.content
+        expect(page).to have_content 'Hello World!'
+        expect(page).to have_content 'Edited'
+      end
+    end
+  end
+
   context 'that is a response' do
     scenario 'is possible' do
       reader = create :reader
