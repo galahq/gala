b98245f34c205de7fccbe2a8355a2bb17949f2f4
Cameron Bothner <cameronbothner@gmail.com>
2019-02-11 17:08:23 -0500

    fix: use module notation to prevent superclass mismatch
    Ahoy::EventsController < ApplicationController
    and Admin::Ahoy::EventsController < Admin::ApplicationController
    
    But writing module Admin
    then class Ahoy::EventsController confused it.

--- app/controllers/admin/ahoy/events_controller.rb
+++ app/controllers/admin/ahoy/events_controller.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 module Admin
-  class Ahoy::EventsController < Admin::ApplicationController
-    def scoped_resource
-      resource_class.order(time: :desc)
+  module Ahoy
+    class EventsController < Admin::ApplicationController
+      def scoped_resource
+        resource_class.order(time: :desc)
+      end
     end
   end
 end
