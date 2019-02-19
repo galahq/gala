4ec27b97382029fc13e5f3fce4d60e1c71153c76
Cameron Bothner <cameronbothner@gmail.com>
2019-02-19 10:51:31 -0500

    chore: add shoulda matchers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -73,6 +73,13 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.include Orchard::Integration::TestHelpers::Authentication, type: :feature
