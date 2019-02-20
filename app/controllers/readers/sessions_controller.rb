801e9df089f310cf407ec778551cc4c6aa2ed23e
Cameron Bothner <cameronbothner@gmail.com>
2019-02-20 13:15:01 -0500

    feat: redirect to edit persona page after first sign in

--- app/controllers/readers/sessions_controller.rb
+++ app/controllers/readers/sessions_controller.rb
@@ -3,6 +3,7 @@
 module Readers
   # Handles signing in and signing out
   class SessionsController < Devise::SessionsController
+    include AfterSignInPath
     include MagicLink
     # before_action :configure_sign_in_params, only: [:create]
 
@@ -32,10 +33,6 @@ module Readers
     # ```
 
     protected
-
-    def after_sign_in_path_for(_resource)
-      after_linking_redirect_path || stored_location_for(:user) || root_path
-    end
 
     # If you have extra params to permit, append them to the sanitizer.
     # def configure_sign_in_params
