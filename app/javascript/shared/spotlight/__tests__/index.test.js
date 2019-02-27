e600bfab8c4a47224a4f1ce3d5bf44d5126816eb
Cameron Bothner <cameronbothner@gmail.com>
2019-02-27 13:01:09 -0500

    feat: style the spotlights

--- app/javascript/shared/spotlight/__tests__/index.test.js
+++ app/javascript/shared/spotlight/__tests__/index.test.js
@@ -7,6 +7,23 @@ import Spotlight from '../index'
 import React from 'react'
 import { fireEvent, render } from 'react-testing-library'
 
+jest.mock('react-popper', () => ({
+  Manager ({ children }) {
+    return children
+  },
+  Reference ({ children }) {
+    return children({ ref: () => {} })
+  },
+  Popper ({ children }) {
+    return children({
+      ref: () => {},
+      style: {},
+      placement: 'bottom',
+      arrowProps: { ref: () => {}, style: {}},
+    })
+  },
+}))
+
 const mockSpotlightManager = mockVisibility => ({
   subscribe: jest.fn((_, setVisibility) => setVisibility(mockVisibility)),
   unsubscribe: jest.fn(),
@@ -23,18 +40,18 @@ describe('Spotlight', () => {
           content="This button does a thing!"
           spotlightKey="test_dummy"
         >
-          button
+          {({ ref }) => <button ref={ref}>button</button>}
         </Spotlight>
       )
-      const { container } = render(component)
+      const { baseElement } = render(component)
 
-      expect(container).not.toHaveTextContent('This button does a thing!')
-      expect(container).toHaveTextContent('button')
+      expect(baseElement).not.toHaveTextContent('This button does a thing!')
+      expect(baseElement).toHaveTextContent('button')
     })
   })
 
   describe('that the user should see', () => {
-    it('includes the content and the target element', () => {
+    it('includes the content and the target element', async () => {
       window.spotlightManager = mockSpotlightManager(true)
 
       const component = (
@@ -42,13 +59,13 @@ describe('Spotlight', () => {
           content="This button does a thing!"
           spotlightKey="test_dummy"
         >
-          button
+          {({ ref }) => <button ref={ref}>button</button>}
         </Spotlight>
       )
-      const { container } = render(component)
+      const { baseElement } = render(component)
 
-      expect(container).toHaveTextContent('This button does a thing!')
-      expect(container).toHaveTextContent('button')
+      expect(baseElement).toHaveTextContent('button')
+      expect(baseElement).toHaveTextContent('This button does a thing!')
     })
 
     it('calls acknowledge on the spotlight manager when the button is clicked', () => {
@@ -59,7 +76,7 @@ describe('Spotlight', () => {
           content="This button does a thing!"
           spotlightKey="test_dummy"
         >
-          button
+          {({ ref }) => <button ref={ref}>button</button>}
         </Spotlight>
       )
       const { getByTestId } = render(component)
