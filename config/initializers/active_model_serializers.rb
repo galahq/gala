e6dbbb9146426205b3bd5599e21e0361e1a20b9c
Cameron Bothner <cameronbothner@gmail.com>
2019-03-14 10:33:29 -0400

    fix: give serializers the view_context/do better when we can’t

--- config/initializers/active_model_serializers.rb
+++ config/initializers/active_model_serializers.rb
@@ -5,7 +5,8 @@ ActiveModelSerializers.config.key_transform = :camel_lower
 ActiveModelSerializers.logger = Logger.new(IO::NULL)
 
 ActiveModel::Serializer.class_eval do
-  def self.for(resource, options = {})
+  def self.for(resource, view_context: nil, **kwargs)
+    options = { scope: view_context, scope_name: :view_context }.merge(kwargs)
     ActiveModelSerializers::SerializableResource.new(resource, options)
   end
 end
