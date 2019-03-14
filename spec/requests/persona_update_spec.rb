b8786bda56fa861054211915a5e501fe15e9e33f
Cameron Bothner <cameronbothner@gmail.com>
2019-03-14 15:10:52 -0400

    feat: redirect to my cases if reader chooses writer persona
    Co-Authored-By: Pearl Zeng <pearlzhuzeng@users.noreply.github.com>

--- spec/requests/persona_update_spec.rb
+++ spec/requests/persona_update_spec.rb
@@ -7,9 +7,18 @@ RSpec.describe 'Persona update' do
     reader = create :reader
     sign_in reader
 
-    patch profile_persona_path, params: { persona: 'teacher' }
+    put profile_persona_path, params: { persona: 'teacher' }
 
     reader.reload
     expect(reader.persona).to eq 'teacher'
+  end
+
+  it 'redirects to My Cases if the user choses writer as their persona' do
+    reader = create :reader
+    sign_in reader
+
+    put profile_persona_path, params: { persona: 'writer' }
+
+    expect(response).to redirect_to my_cases_path
   end
 end
