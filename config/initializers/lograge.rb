0a0920fd65dcf816502c311f703c50f61dc31e31
Cameron Bothner <cameronbothner@gmail.com>
2019-03-19 17:52:54 -0400

    fix: include params in logs

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -2,7 +2,14 @@
 
 Rails.application.configure do
   config.lograge.enabled = true
+
+  config.lograge.custom_options = ->(event) do
+    exceptions = %w[controller action format id]
+    { params: event.payload[:params].except(*exceptions) }
+  end
+
   config.lograge.formatter = ->(data) {
-    data.awesome_inspect multiline: false
+    details = data.awesome_inspect multiline: !Rails.env.production?
+    "👋  #{details}"
   }
 end
