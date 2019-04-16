4cacb732a7026a7141123465c113145213a62b84
Cameron Bothner <cameronbothner@gmail.com>
2019-04-16 16:38:05 -0400

    fix: make it possible to set a ReadingListItem’s case from a slug

--- app/models/reading_list_item.rb
+++ app/models/reading_list_item.rb
@@ -7,14 +7,27 @@
 class ReadingListItem < ApplicationRecord
   default_scope -> { order :position }
 
+  attribute :case_slug, type: :string
+
   belongs_to :case
   belongs_to :reading_list
 
-  acts_as_list scope: :reading_list
+  after_find :initialize_case_slug
+  before_validation :set_case_from_slug, if: :case_slug_changed?
 
-  delegate :slug, to: :case, prefix: true
+  acts_as_list scope: :reading_list
 
   def enrolled?(reader)
     reader.enrollment_for_case(self.case)
+  end
+
+  private
+
+  def initialize_case_slug
+    self.case_slug = self.case&.slug
+  end
+
+  def set_case_from_slug
+    self.case = Case.friendly.find(case_slug)
   end
 end
