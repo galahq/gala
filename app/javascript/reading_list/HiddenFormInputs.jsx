cd3ea343b0d4fb9263f4556e1978f1e9b5016969
Cameron Bothner <cameronbothner@gmail.com>
2019-04-16 14:34:10 -0400

    feat: render hidden form inputs for nested reading_list_items attrs
    Co-Authored-By: Pearl Zhu Zeng <zhuzeng@umich.edu>

---
+++ app/javascript/reading_list/HiddenFormInputs.jsx
@@ -0,0 +1,45 @@
+/**
+ * @providesModule HiddenFormInputs
+ * @flow
+ */
+
+import * as React from 'react'
+
+import type { ReadingListItem } from 'redux/state'
+
+type Props = {
+  items: ReadingListItem[],
+}
+
+function HiddenFormInputs ({ items }: Props): React.Node[] {
+  return items.map(({ caseSlug, notes, param }, index) => (
+    <React.Fragment key={caseSlug}>
+      <input
+        readOnly
+        name={`reading_list[reading_list_items_attributes][${index}][case_slug]`}
+        type="hidden"
+        value={caseSlug}
+      />
+      <input
+        readOnly
+        name={`reading_list[reading_list_items_attributes][${index}][notes]`}
+        type="hidden"
+        value={notes}
+      />
+      <input
+        readOnly
+        name={`reading_list[reading_list_items_attributes][${index}][param]`}
+        type="hidden"
+        value={param}
+      />
+      <input
+        readOnly
+        name={`reading_list[reading_list_items_attributes][${index}][position]`}
+        type="hidden"
+        value={index}
+      />
+    </React.Fragment>
+  ))
+}
+
+export default HiddenFormInputs
