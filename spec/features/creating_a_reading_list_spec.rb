aac313a4fe62440191abf68db1b20058293ebde8
Cameron Bothner <cameronbothner@gmail.com>
2019-04-17 13:29:13 -0400

    test: test creating a reading list with items

--- spec/features/creating_a_reading_list_spec.rb
+++ spec/features/creating_a_reading_list_spec.rb
@@ -14,18 +14,24 @@ feature 'Creating a reading list' do
     click_on 'New Reading List'
 
     fill_in 'Title', with: 'My Reading List'
-    fill_in 'Description', with: 'These are cool cases.'
+    fill_in 'Description', with: 'These are cool cases'
 
-    # TODO: Add a case
+    click_on 'Add'
+    fill_in 'Notes', with: 'I think this is cool'
 
     click_on 'Create Reading List'
 
     expect(page).to have_content 'My Reading List'
-    expect(page).to have_content 'These are cool cases.'
+    expect(page).to have_content 'These are cool cases'
+    expect(page).to have_content 'A Cool Case'
+    expect(page).to have_content 'I think this is cool'
 
     reader.reload
     list = reader.reading_lists.order(:created_at).last
     expect(list.title).to eq 'My Reading List'
-    expect(list.description).to eq 'These are cool cases.'
+    expect(list.description).to eq 'These are cool cases'
+    expect(list.reading_list_items.where(notes: 'I think this is cool'))
+      .not_to be_empty
+    expect(list.cases).to include kase
   end
 end
