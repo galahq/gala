bbb52753df9ce151ee89dce55b1219810d147eba
Cameron Bothner <cameronbothner@gmail.com>
2019-04-17 14:32:32 -0400

    fix: use simple getter and setter for ReadingListItem#case_slug
    Setting case_slug in an after_find hook is too early to benefit from
    eager loading. Using a simple cached getter is lazy and avoid the N+1
    query. It does mean that the setter is eager by default, which is
    probably fine.

--- app/models/reading_list_item.rb
+++ app/models/reading_list_item.rb
@@ -7,27 +7,22 @@
 class ReadingListItem < ApplicationRecord
   default_scope -> { order :position }
 
-  attribute :case_slug, type: :string
-
   belongs_to :case
   belongs_to :reading_list
 
-  after_find :initialize_case_slug
-  before_validation :set_case_from_slug, if: :case_slug_changed?
-
   acts_as_list scope: :reading_list
+
+  def case_slug
+    @case_slug ||= self.case&.slug
+  end
+
+  def case_slug=(slug)
+    return if slug == self.case&.slug
+
+    self.case = Case.friendly.find slug
+  end
 
   def enrolled?(reader)
     reader.enrollment_for_case(self.case)
-  end
-
-  private
-
-  def initialize_case_slug
-    self.case_slug = self.case&.slug
-  end
-
-  def set_case_from_slug
-    self.case = Case.friendly.find(case_slug)
   end
 end
