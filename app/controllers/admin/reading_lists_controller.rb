662d226ae5694549d04246373d96cd4f258f2696
Cameron Bothner <cameronbothner@gmail.com>
2019-04-23 11:53:07 -0400

    feat: add reading lists to admin panel

---
+++ app/controllers/admin/reading_lists_controller.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Admin
+  class ReadingListsController < Admin::ApplicationController
+    # To customize the behavior of this controller,
+    # you can overwrite any of the RESTful actions. For example:
+    #
+    # def index
+    #   super
+    #   @resources = ReadingList.
+    #     page(params[:page]).
+    #     per(10)
+    # end
+
+    # Define a custom finder by overriding the `find_resource` method:
+    def find_resource(param)
+      ReadingList.find_by!(uuid: param)
+    end
+
+    # See https://administrate-prototype.herokuapp.com/customizing_controller_actions
+    # for more information
+  end
+end
