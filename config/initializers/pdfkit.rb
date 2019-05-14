bd3b2fb0fb8507fba0f9a384ad53f175b61400fa
Cameron Bothner <cameronbothner@gmail.com>
2019-05-14 11:40:29 -0400

    fix: remove pdfkit middleware and configure it for use from jobs

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
-options = {
-  dpi: 300,
-  margin_left: '1.5in',
-  margin_right: '1.5in'
-}
-
-Rails.application.config
-     .middleware
-     .use PDFKit::Middleware, options, only: %r{cases/[a-z0-9-]+/archive}
+PDFKit.configure do |config|
+  config.default_options = {
+    dpi: 300,
+    margin_left: '1.5in',
+    margin_right: '1.5in'
+  }
+end
