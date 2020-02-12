20a6df86a3aae13de48cddf8154acff1b24f7661
timothyf <timothyf@gmail.com>
2020-02-12 17:44:59 -0500

    fixed translation errors that occured while duplicating activestorage attachements

--- app/cloners/edgenote_cloner.rb
+++ app/cloners/edgenote_cloner.rb
@@ -9,6 +9,15 @@ class EdgenoteCloner < Clowne::Cloner
 
   finalize do |_source, record, kase:, **|
     record.case = kase
+    if _source.audio.attached? && _source.audio.blob
+      record.audio.attach(_source.audio.blob)
+    end
+    if _source.file.attached? && _source.file.blob
+      record.file.attach(_source.file.blob)
+    end
+    if _source.image.attached? && _source.image.blob
+      record.image.attach(_source.image.blob)
+    end
     record.save!
   end
 end
