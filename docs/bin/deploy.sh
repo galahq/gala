01773038dd32cff9504406d867b5058fcb24e3a1
waisaed <waisaed@umich.edu>
2021-07-07 13:07:35 -0400

    add deploy script back in

---
+++ docs/bin/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+yarn build
+aws s3 sync --acl public-read --sse --delete ./build/gala-docs/ \
+  s3://docs.learngala.com
+aws configure set preview.cloudfront true
+aws cloudfront create-invalidation --distribution-id E2EDUP84FXRXCP --paths '/*'
