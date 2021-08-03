adafd9b8d0ac2c7aa0f5f161bfa483ddbb20137c
waisaed <waisaed@umich.edu>
2021-08-03 15:02:09 -0400

    change build directory to v2 path

--- docs/website/bin/deploy.sh
+++ docs/website/bin/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yarn build
-aws s3 sync --acl public-read --sse --delete ./build/gala-docs/ \
+aws s3 sync --acl public-read --sse --delete ./build/ \
   s3://docs.learngala.com
 aws configure set preview.cloudfront true
 aws cloudfront create-invalidation --distribution-id E2EDUP84FXRXCP --paths '/*'
