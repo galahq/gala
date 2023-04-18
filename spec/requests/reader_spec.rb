7bb886cc3cb034367547f6f9a53f8f1e10b45821
Alex Fichter <afichter@umich.edu>
2023-04-18 20:05:12 +0000

    tweaks, test improvements

--- spec/requests/reader_spec.rb
+++ spec/requests/reader_spec.rb
@@ -4,11 +4,47 @@ require 'rails_helper'
 
 RSpec.describe 'Reader' do
 
+  context 'edit tos' do
+    it 'visits :terms_of_service form' do
+      reader = create :reader, terms_of_service: nil
+      sign_in reader
+      get edit_tos_reader_path(reader), params: { use_route: 'readers/edit_tos', id: reader.id }
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'accepts :terms_of_service' do
+      reader = create :reader, terms_of_service: nil
+      sign_in reader
+      terms_of_service = '1'
+      post update_tos_reader_path(reader), params: { id: reader, reader: { terms_of_service: terms_of_service } }
+      reader.reload
+      expect(reader.terms_of_service).to eq 1
+    end
+  end
+
   it 'redirects to :terms_of_service when nil' do
     reader = create :reader, terms_of_service: nil
     sign_in reader
     get cases_url
     expect(response).to redirect_to(edit_tos_reader_path(reader))
+  end
+
+  it 'does not redirect when terms_of_servie is populated' do
+    reader = create :reader, terms_of_service: 1
+    sign_in reader
+    get cases_url
+    expect(response).to have_http_status(:success)
+  end
+
+  it 'continues to saved forwarding_url after submission' do
+    reader = create :reader, terms_of_service: nil
+    sign_in reader
+    terms_of_service = '1'
+    destination_url = cases_url
+    get destination_url
+    expect(response).to redirect_to(edit_tos_reader_path(reader))
+    post update_tos_reader_url(reader), params: { id: reader, reader: { terms_of_service: terms_of_service } }
+    expect(response).to redirect_to destination_url
   end
   
 end
