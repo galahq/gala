d9ac0b6894d81e505ac40e00fa9d14ec14c568ac
Nathan Papes <nathan.papes@gmail.com>
2023-08-23 13:56:52 -0400

    setup test db as part of the entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,9 @@
 if [ "$RAILS_ENV" = "development" ]; then
   if [ ! -f /tmp/seeded.txt ] && [ "$1" = "foreman" ]; then
     touch /tmp/seeded.txt
-    bundle exec rails db:seed > /dev/null 2>&1
+    bundle exec rails db:environment:set RAILS_ENV=development
+    bundle exec rails db:seed
+    bundle exec rails db:test:prepare
   fi
 fi
 
