4f68078e1b820685d9e5de1ad71b04be22b50620
Alex Fichter <afichter@umich.edu>
2023-09-10 16:06:34 -0400

    spec sign in and config

--- spec/requests/deployment_spec.rb
+++ spec/requests/deployment_spec.rb
@@ -4,7 +4,17 @@ require 'rails_helper'
 
 RSpec.describe "/deployments", type: :request do
   it "destroys a deployment" do
-    @deployment = create(:deployment, :with_quiz)
+    kase = create :case
+    reader = create :reader
+    create :enrollment, case: kase, reader: reader
+    reader.my_cases << kase
+
+    my_group = create :group, name: 'My Group'
+    create :group_membership, :admin, group: my_group, reader: reader
+    @deployment = create :deployment, :with_quiz, group: my_group, case: kase
+
+    sign_in reader
+
     expect{delete deployment_url(@deployment)}.to change{Deployment.count}.by(-1)
   end
 end
