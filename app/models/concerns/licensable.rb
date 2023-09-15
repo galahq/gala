898a35f8be8adaf4ca98361e2830e89ec7857999
Nathan Papes <nathan.papes@gmail.com>
2023-09-15 16:29:39 -0400

    fix rubocop issues

--- app/models/concerns/licensable.rb
+++ app/models/concerns/licensable.rb
@@ -6,23 +6,23 @@ module Licensable
 
   included do
     validates :license, inclusion: {
-      in: -> (record) { record.available_licenses.map{|o| o['id'] } },
-      message: "%{value} is not a valid license"
+      in: ->(record) { record.available_licenses.map { |o| o['id'] } },
+      message: '%<value>s is not a valid license'
     }
   end
 
   def available_licenses
-    LICENSES.select do |license|
-      has_grandfathered_license? || license['active'] == true
+    LICENSES.select do |o|
+      grandfathered_license? || o['active'] == true
     end
   end
 
-  def has_grandfathered_license?
+  def grandfathered_license?
     !license_changed? && license == 'all_rights_reserved'
   end
 
   def license_config
-    conf = LICENSES.find { |l| l['id'] == license }
+    conf = LICENSES.find { |o| o['id'] == license }
     conf['icon_path'] = ActionController::Base.helpers.asset_path(
       "licenses/#{conf['icon']}"
     ) rescue nil
