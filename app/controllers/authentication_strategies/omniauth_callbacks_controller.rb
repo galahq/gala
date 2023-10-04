e21578f09ca06a9e7b7c87e7079ca2703a25e70d
Nathan Papes <8855672+papes1ns@users.noreply.github.com>
2023-10-04 13:45:36 -0400

    Clean up exired locks (#687)

--- app/controllers/authentication_strategies/omniauth_callbacks_controller.rb
+++ app/controllers/authentication_strategies/omniauth_callbacks_controller.rb
@@ -5,6 +5,7 @@ module AuthenticationStrategies
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     include AfterSignInPath
     include MagicLink
+    include CleanupLocks
 
     skip_before_action :verify_authenticity_token
     before_action :set_authentication_strategy, except: [:failure]
@@ -14,6 +15,7 @@ module AuthenticationStrategies
     def google
       if @authentication_strategy.persisted?
         sign_in @reader
+        enqueue_cleanup_locks_job
         link_reader if following_magic_link?
         redirect_to after_sign_in_path_for @reader
       else
@@ -24,9 +26,10 @@ module AuthenticationStrategies
     end
 
     # @route [GET, POST] `/authentication_strategies/auth/lti/callback`
-    def lti
+    def lti # rubocop:disable Metrics/AbcSize
       if @authentication_strategy.persisted?
         sign_in @reader
+        enqueue_cleanup_locks_job
 
         linker = LinkerService.new LinkerService::LTIStrategy.new params
         linker.call
---
+++ lib/tasks/locks.rake
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+namespace :locks do
+  desc 'Enqueue CleanupLocksJob for a specific reader or old locks'
+  task enqueue_cleanup: :environment do
+    reader_id = ENV['READER_ID']
+    destroy_after = ENV['DESTROY_AFTER'] || 8.hours.to_i
+
+    CleanupLocksJob.perform_later(reader_id: reader_id,
+                                  destroy_after: destroy_after)
+  end
+end
