8ac7bcb56fe2d3f6ff3abadbfc4df944ff8eb169
Alex Fichter <afichter@umich.edu>
2023-12-10 14:12:31 -0500

    spec syntax

--- spec/features/editing_a_case_spec.rb
+++ spec/features/editing_a_case_spec.rb
@@ -96,11 +96,11 @@ feature 'Editing a case' do
 
     context 'with a comment thread' do
       let!(:kase) { create :case_with_edgenotes }
-      let!(:enrollment) {create :enrollment, case: kase, reader: reader}
+      let!(:enrollment) {create(:enrollment, case: kase, reader: reader)}
 
-      let!{:group} {create :group, name: 'My Group'}
-      let!(:group_membership) {create :group_membership, group: group, reader: reader}
-      let!(:deployment) {create :deployment, group: group, case: kase}
+      let!{:group} {create(:group, name: 'My Group')}
+      let!(:group_membership) {create(:group_membership, group: group, reader: reader)}
+      let!(:deployment) {create(:deployment, group: group, case: kase)}
       
       let!(:comment_thread) do
         card = kase.pages.first.cards.first
