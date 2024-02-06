a07264fa45be941187b96651a6f6ea5d00f90adf
Alex Fichter <afichter@umich.edu>
2024-02-06 16:47:03 -0500

    wip, table

--- app/helpers/cases_helper.rb
+++ app/helpers/cases_helper.rb
@@ -11,4 +11,14 @@ module CasesHelper
   def cases_as_json(cases)
     array = cases.map { |c| raw render partial: 'cases/case', formats: [:json], locals: { c: c } }
   end
+
+  def duration(cases)
+    if cases.present?
+      time = cases.select("properties->>'duration' as duration")&.map{|r|r.duration.to_i}&.map{|d|d/60}&.then { |a| a.sum.to_f / a.size }.round
+      "#{time} seconds"
+    else
+      "NA"
+    end
+
+  end
 end
