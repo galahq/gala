2853992eb0383250e27113dcd7305ead7430425e
Nathan Papes <nathan.papes@gmail.com>
2024-02-14 12:52:24 -0500

    wip MathEntity improve the SVG toggle back to text

--- app/javascript/draft/MathEntity.jsx
+++ app/javascript/draft/MathEntity.jsx
@@ -3,11 +3,41 @@
  * @noflow
  */
 
-import React from 'react'
+import React, { useState } from 'react'
 import Tex2SVG from "react-hook-mathjax"
 
-const MathEntity = ({ decoratedText }) => {
-  return <Tex2SVG display="inline" latex={decoratedText} />
+export function MathEntity (props) {
+  const { decoratedText: latex, editorState, contentState, entityKey, children } = props
+
+  const [error, setError] = useState(null)
+  const [isSvg, setIsSvg] = useState(true)
+
+  if (error) {
+    console.log({ error })
+  }
+
+  const toggleSvg = () => {
+    setIsSvg(!isSvg)
+  }
+
+  if (isSvg) {
+    return (
+      <div onClick={toggleSvg}>
+        <Tex2SVG
+          latex={latex}
+          display="inline-block"
+          onSuccess={() => setError(null)}
+          onError={setError}
+        />
+      </div>
+    )
+  } else {
+    return (
+      <div onClick={toggleSvg}>
+        {latex}
+      </div>
+    )
+  }
 }
 
 export default MathEntity
