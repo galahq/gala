ab135445fbf1c2419b004bf3081c2aa66e1106cc
Nathan Papes <8855672+papes1ns@users.noreply.github.com>
2024-03-13 12:55:05 -0400

    Feat revealable draftjs entity (#697)

--- app/javascript/draft/config.js
+++ app/javascript/draft/config.js
@@ -10,6 +10,7 @@ import EdgenoteEntity from './EdgenoteEntity'
 import CitationEntity from './CitationEntity'
 import LinkEntity from './LinkEntity'
 import CommentThreadEntity from 'comments/CommentThreadEntity'
+import RevealableEntity from './RevealableEntity'
 
 export const styles = {
   smallCaps: {
@@ -112,6 +113,10 @@ export const decorator = new CompositeDecorator([
   {
     strategy: getFindEntityFunction('LINK'),
     component: LinkEntity,
+  },
+  {
+    strategy: getFindEntityFunction('REVEALABLE'),
+    component: RevealableEntity,
   },
 ])
 
