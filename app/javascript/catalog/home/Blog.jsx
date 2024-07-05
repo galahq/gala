12d9b3078c73007ded759138b95bd03ee320b4c3
waisaed <waisaed@umich.edu>
2024-07-05 10:44:05 -0400

    cleanup blog component

--- app/javascript/catalog/home/Blog.jsx
+++ app/javascript/catalog/home/Blog.jsx
@@ -1,14 +1,14 @@
-import * as React from 'react'
+import React, {useState, useEffect} from 'react'
 
 export default function Blog() {
-  const [feed, setFeed] = React.useState([])
-  React.useEffect(() => {
+  const [feed, setFeed] = useState([])
+  useEffect(() => {
     fetch('https://corsproxy.io/?https://docs.learngala.com/blog/rss.xml', {})
       .then(response => response.text())
       .then(text => {
         const parser = new DOMParser()
         const doc = parser.parseFromString(text, 'text/xml')
-        handle_feed(doc)
+        setFeed(handle_feed(doc))
       })
       .catch(error => {
         console.log(error)
@@ -16,47 +16,20 @@ export default function Blog() {
   }, [])
 
   function handle_feed(doc) {
+    let parsedFeed = [];
     for (let i = 0; i < doc.querySelectorAll('item').length; i++) {
       let item = doc.querySelectorAll('item')[i]
       let title = item.querySelector('title').textContent
       let link = item.querySelector('link').textContent
-      setFeed(feed => [...feed, {title, link}])
+      parsedFeed.push({title, link});
     }
+    return parsedFeed;
   }
-    function FeedElement({ title, link}) {
-    return (
-      <div>
-        <a
-          href={link}
-          className='feedLink'
-        >
-          {title}
-        </a>
-      </div>
-    )
-  }
-  function RenderedFeed({ feed }) {
-    return (<>
-    <ul>
-    {feed.map(
-      (item, index) =>
-        index < 4 && (
-          <li key={index}>
-          <FeedElement
-            key={index}
-            title={item.title}
-            link={item.link}
-          />
-          </li>
-    ))}
-    </ul>
-    </>)
-  }
-
+    
   return (
     <>
       <div className='blog'>
-        <h4>Recent Updates</h4>
+        <h2>Recent Updates</h2>
         <RenderedFeed className='feed' feed={feed} />
         <div class='moreLink'>
           <a 
@@ -70,4 +43,35 @@ export default function Blog() {
       </div>
     </>
   )
+}
+
+function FeedElement({ title, link}) {
+  return (
+    <div>
+      <a
+        href={link}
+        className='feedLink'
+      >
+        {title}
+      </a>
+    </div>
+  )
+}
+
+function RenderedFeed({ feed }) {
+  return (<>
+  <ul>
+  {feed.map(
+    (item, index) =>
+      index < 4 && (
+        <li key={index}>
+        <FeedElement
+          key={index}
+          title={item.title}
+          link={item.link}
+        />
+        </li>
+  ))}
+  </ul>
+  </>)
 }
