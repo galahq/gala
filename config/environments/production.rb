3c9ab2fd51151cb6569bd6358be06e1e184a5014
Nathan Papes <nathan.papes@gmail.com>
2024-12-22 19:26:53 -0500

    make use of BASE_URL in production.rb file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,6 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.action_controller.enable_fragment_cache_logging = true
 
   # Ensures that a master key has been made available in either
   # ENV["RAILS_MASTER_KEY"] or in config/master.key. This key is used to decrypt
@@ -60,10 +59,10 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Action Cable endpoint configuration
-  config.action_cable.url = 'wss://www.learngala.com/cable'
-  config.action_cable.allowed_request_origins = %w[
-    http://www.learngala.com
-    https://www.learngala.com
+  config.action_cable.url = "wss://#{BASE_URL_HOST}/cable"
+  config.action_cable.allowed_request_origins = [
+    "http://#{BASE_URL_HOST}",
+    "https://#{BASE_URL_HOST}"
   ]
 
   # Store uploaded files on the local file system (see config/storage.yml for
