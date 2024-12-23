bcf0bcbb58532cd7da79641ca0caf2fe606405ab
waisaed <waisaed@umich.edu>
2024-12-23 14:10:43 -0500

    add youtu.be embed ratio

--- app/javascript/edgenotes/expansion/index.jsx
+++ app/javascript/edgenotes/expansion/index.jsx
@@ -43,6 +43,7 @@ const EMBED_RATIOS: { [string]: [number, number] } = {
   'ted.com': [16, 9],
   'vimeo.com': [16, 9],
   'youtube.com': [16, 9],
+  'youtu.be': [16, 9],
   'datastudio.google.com': [1, 1],
   'observablehq.com': [16, 9],
   'crowdsignal.com': [1, 1],
