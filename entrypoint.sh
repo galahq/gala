139be5b0ace03c222f7bca9e1545982181fff5bf
Nathan Papes <nathan.papes@gmail.com>
2025-01-05 14:33:20 -0500

    fix number comparision in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ if [ "$RAILS_ENV" = "development" ]; then
   if [ ! -f /tmp/.seeded ]; then
     touch /tmp/.seeded
     count=$(bundle exec rails runner "puts Case.count" | tail -n 1)
-    if [ "$count" -eq 0 ]; then
+    if [ "$count" -eq "0" ]; then
       bundle exec rails db:environment:set RAILS_ENV=development
       bundle exec rails db:seed
       bundle exec rails db:test:prepare
