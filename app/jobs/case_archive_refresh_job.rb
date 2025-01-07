3be04e85a2e7688d77af9cd14d99919587cece08
waisaed <waisaed@umich.edu>
2025-01-06 22:34:49 -0500

    add current date to archive filename

--- app/jobs/case_archive_refresh_job.rb
+++ app/jobs/case_archive_refresh_job.rb
@@ -4,7 +4,7 @@
 class CaseArchiveRefreshJob < ActiveJob::Base
   def perform(archive, root_url:)
     pdf = Case::Pdf.new(archive.case, root_url: root_url).file
-    filename = "#{archive.case.slug}.pdf"
+    filename = "#{archive.case.slug} - #{Date.today}.pdf"
     archive.pdf.attach io: StringIO.new(pdf), filename: filename
   end
 end
