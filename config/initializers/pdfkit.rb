2ea012c6d2e78738768e97f7b6729f175e83fddd
Nathan Papes <nathan.papes@gmail.com>
2025-01-09 12:00:05 -0500

    wkhtmltopdf issue

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 PDFKit.configure do |config|
-  config.wkhtmltopdf = '/usr/bin/wkhtmltopdf'
+  config.wkhtmltopdf = '/usr/bin/wkhtmltopdf' unless Rails.env.production?
   config.default_options = {
     dpi: 300
   }
