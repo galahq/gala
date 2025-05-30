18050ff5ef04d661edd58d81b9ae768568c35ea9
Nathan Papes <nathan.papes@gmail.com>
2025-05-29 21:50:27 -0400

    default new deployment to pre/post quiz by default

--- app/javascript/deployment/index.jsx
+++ app/javascript/deployment/index.jsx
@@ -146,7 +146,7 @@ class Deployment extends React.Component<Props, State> {
     )
     this.state = {
       selectedQuizId: props.selectedQuizId,
-      answersNeeded: props.answersNeeded || 1,
+      answersNeeded: props.answersNeeded || 2,
       customQuestions,
     }
   }
