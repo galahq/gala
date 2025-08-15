25d5e3bca8398ff2970294d563ee874d3f05892c
Nathan Papes <nathan.papes@gmail.com>
2025-08-15 16:01:52 -0400

    Pumas, routers and keep alives

--- config/puma.rb
+++ config/puma.rb
@@ -3,27 +3,30 @@
 require 'barnes'
 require 'puma_worker_killer'
 
-persistent_timeout ENV.fetch('PERSISTENT_TIMEOUT', 20).to_i
-
-max_threads_count = ENV.fetch('MAX_THREADS', 5).to_i
-min_threads_count = ENV.fetch('MIN_THREADS') { max_threads_count }.to_i
+environment ENV.fetch('RAILS_ENV', 'production')
+workers ENV.fetch('WEB_CONCURRENCY', 2).to_i
+max_threads_count = ENV.fetch('MAX_RAILS_THREADS', 5).to_i
+min_threads_count = ENV.fetch('MIN_RAILS_THREADS', 5).to_i
 threads min_threads_count, max_threads_count
 
-bind "tcp://0.0.0.0:#{ENV.fetch('PORT', 3000)}"
+# https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server
+enable_keep_alives(false) if respond_to?(:enable_keep_alives)
+persistent_timeout ENV.fetch('PERSISTENT_TIMEOUT', 20).to_i
 
-environment ENV.fetch('RAILS_ENV', 'production')
-workers     ENV.fetch('WEB_CONCURRENCY', 2).to_i
+worker_timeout ENV.fetch('WORKER_TIMEOUT', 25).to_i
+worker_shutdown_timeout ENV.fetch('WORKER_SHUTDOWN_TIMEOUT', 8).to_i
+
+bind "tcp://0.0.0.0:#{ENV.fetch('PORT', 3000)}"
 
 preload_app!
 
 before_fork do
   PumaWorkerKiller.config do |config|
-    config.ram =                       2048 # mb (2GB)
-    config.frequency =                 3600 # seconds (1 hour)
-    config.percent_usage             = 0.98
-    config.rolling_restart_frequency = 6.hours.to_i
-    config.reaper_status_logs = true
-    config.pre_term = ->(worker) { puts "🛑 SIGTERM-inated PID##{worker.pid}" }
+    config.ram                        = 1024 + 512
+    config.frequency                  = 10.minutes.to_i
+    config.percent_usage              = 0.85
+    config.rolling_restart_frequency  = 12.hours.to_i
+    config.reaper_status_logs         = true
   end
   PumaWorkerKiller.start
   Barnes.start
