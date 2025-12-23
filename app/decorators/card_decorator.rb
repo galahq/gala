0cf5f58f8360c701041f5fd51072942bacbd0efd
Nathan Papes <nathan.papes@gmail.com>
2025-12-22 21:25:04 -0500

    handle missing edgenote slugs more gracefully

--- app/decorators/card_decorator.rb
+++ app/decorators/card_decorator.rb
@@ -5,11 +5,31 @@ class CardDecorator < ApplicationDecorator
   # Find the edgenote objects from the case’s association to take advantage of
   # preloading
   def edgenotes
-    edgenote_slugs.map do |slug|
-      object.case.edgenotes.find do |edgenote|
-        edgenote.slug == slug
-      end
-    end.map(&:decorate)
+    missing_slugs = []
+    edgenotes = find_edgenotes(missing_slugs)
+    log_missing_slugs(missing_slugs) if missing_slugs.any?
+    edgenotes.map(&:decorate)
+  end
+
+  private
+
+  def find_edgenotes(missing_slugs)
+    edgenote_slugs.filter_map do |slug|
+      edgenote =
+        object.case.edgenotes.find do |candidate|
+          candidate.slug == slug
+        end
+
+      missing_slugs << slug unless edgenote
+      edgenote
+    end
+  end
+
+  def log_missing_slugs(slugs)
+    Rails.logger.warn(
+      "CardDecorator#edgenotes missing slugs=#{slugs.join(',')} " \
+      "card_id=#{object.id} case_id=#{object.case_id}"
+    )
   end
 
   def edgenote_slugs
