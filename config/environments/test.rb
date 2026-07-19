f62cf921ec73ec3ab4a9e380102d5cf41d392ceb
Nathan Papes <nathan.papes@gmail.com>
2026-07-18 22:44:50 -0400

    fix: precompile all rails test assets

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -21,6 +21,7 @@ Rails.application.configure do
   config.public_file_server.headers = {
     'Cache-Control' => "public, max-age=#{1.hour.to_i}"
   }
+  config.assets.css_compressor = nil
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
